b378cae65bef7e8199c53ef712434b73511df385
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-07-04 00:21:51 +0530

    Time: 2353 ms (5.00%), Space: 51.1 MB (24.41%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,22 +1,47 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
+        
+        // Approach 1 - Sorting
+        // TC- O(nlogn + n)     SC- O(1)
+      
+      // int n = nums.size();
+      // if(nums.empty()){
+      //   return 0;
+      // }
+      // sort(nums.begin(), nums.end());
+      // int ans = 0, temp = 0;
+      // for(int i = 0; i < n-1; i++){
+      //   if(nums[i+1] - nums[i] == 1){
+      //     temp++;
+      //   } else if(nums[i+1] == nums[i]){
+      //     continue;
+      //   } else {
+      //     temp = 0;
+      //   }
+      //   ans = max(temp, ans);
+      // }
+      // return ans+1;
+      
+        // Approach 2 - HashSet
+        // TC- O(n)     SC- O(n)
+      
       int n = nums.size();
-      if(nums.empty()){
-        return 0;
-      }
-      sort(nums.begin(), nums.end());
-      int ans = 0, temp = 0;
-      for(int i = 0; i < n-1; i++){
-        if(nums[i+1] - nums[i] == 1){
-          temp++;
-        } else if(nums[i+1] == nums[i]){
-          continue;
-        } else {
-          temp = 0;
+      set<int> hashSet(nums.begin(), nums.end());
+      
+      int ans = 0;
+      
+      for(auto num : nums){
+        if(!hashSet.count(num-1)){
+          int cnt = 1;
+          int num2 = num;
+          while(hashSet.count(num2+1)){
+            cnt++;
+            num2++;
+          }
+          ans = max(ans, cnt);
         }
-        ans = max(temp, ans);
       }
-      return ans+1;
+      return ans;      
     }
 };
