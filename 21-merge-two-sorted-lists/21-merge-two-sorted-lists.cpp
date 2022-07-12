a2fd3129b7833d0513c190d5378fd7d64a418ba4
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-07-12 20:33:05 +0530

    Time: 14 ms (40.59%), Space: 14.9 MB (42.41%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,7 +11,7 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-      // Recursive Approach
+      // Recursive Approach  TC- O(n+m) SC- O(1)
       
 //       if(l1 == nullptr)
 //         return l2;
@@ -28,21 +28,50 @@ public:
 //       }
 //       return l1;
       
-      // Iterative Approach
+      // Iterative Approach  TC- O(n+m) SC- O(1)
       
+      // if(l1 == nullptr) return l2;
+      // if(l2 == nullptr) return l1;
+      // if(l1->val > l2->val) std::swap(l1, l2);
+      // ListNode *res = l1;
+      // while(l1 != nullptr and l2 != nullptr){
+      //   ListNode *temp = nullptr;
+      //   while(l1 != nullptr and l1->val <= l2->val){
+      //     temp = l1;
+      //     l1 = l1->next;
+      //   }
+      //   temp->next = l2;
+      //   std::swap(l1, l2);
+      // }
+      // return res;
+      
+      
+      // Extra Space Solution
+      // TC- O(n+m) SC- O(n+m)
       if(l1 == nullptr) return l2;
-      if(l2 == nullptr) return l1;
-      if(l1->val > l2->val) std::swap(l1, l2);
-      ListNode *res = l1;
-      while(l1 != nullptr and l2 != nullptr){
-        ListNode *temp = nullptr;
-        while(l1 != nullptr and l1->val <= l2->val){
-          temp = l1;
-          l1 = l1->next;
-        }
-        temp->next = l2;
-        std::swap(l1, l2);
+      if(l2 == nullptr) return l1;   
+      ListNode *ans = l1;
+      if(l1->val > l2->val){
+        ans = l2;
+        l2 = l2->next;
+      } else {
+        l1 = l1->next;
       }
-      return res;
+      ListNode *temp = ans;
+      while(l1 != nullptr and l2 != nullptr){
+        if(l1->val < l2->val){
+          temp->next = l1;
+          l1 = l1->next;
+        } else {
+          temp->next = l2;
+          l2 = l2->next;
+        }
+        temp = temp->next;
+      }
+      if(l1 != nullptr)
+        temp->next = l1;
+      else 
+        temp->next = l2;
+      return ans;      
     }
 };
