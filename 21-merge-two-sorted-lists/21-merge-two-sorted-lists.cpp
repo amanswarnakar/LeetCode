e85da6b332c92d06b36ddd173edd64a2b5d7461d
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-07-12 19:24:18 +0530

    Time: 15 ms (35.79%), Space: 14.7 MB (81.06%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,19 +11,38 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-      if(l1 == nullptr)
-        return l2;
-      if(l2 == nullptr)
-        return l1;
+      // Recursive Approach
       
-      if(l1->val < l2->val){
-        l1->next = mergeTwoLists(l1->next, l2);
-        return l1;
+//       if(l1 == nullptr)
+//         return l2;
+//       if(l2 == nullptr)
+//         return l1;
+      
+//       if(l1->val < l2->val){
+//         l1->next = mergeTwoLists(l1->next, l2);
+//         return l1;
+//       }
+//       else{
+//         l2->next = mergeTwoLists(l1, l2->next);
+//         return l2;
+//       }
+//       return l1;
+      
+      // Iterative Approach
+      
+      if(l1 == nullptr) return l2;
+      if(l2 == nullptr) return l1;
+      if(l1->val > l2->val) std::swap(l1, l2);
+      ListNode *res = l1;
+      while(l1 != nullptr and l2 != nullptr){
+        ListNode *temp(0);
+        while(l1 != nullptr and l1->val <= l2->val){
+          temp = l1;
+          l1 = l1->next;
+        }
+        temp->next = l2;
+        std::swap(l1, l2);
       }
-      else{
-        l2->next = mergeTwoLists(l1, l2->next);
-        return l2;
-      }
-      return l1;
+      return res;
     }
 };
