949a3b998ad4c468b910f1893abf012fb786ec76
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-07-18 00:03:51 +0530

    Time: 61 ms (64.39%), Space: 14.5 MB (72.39%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,44 +9,53 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-      int a = 1, b = 1;
-      ListNode *p = headA;
-      ListNode *q = headB;
-      while(p->next){
-        a++;
-        p = p->next;
-      }
-      while(q->next){
-        b++;
-        q = q->next;
-      }
-//       if(p != q){
-//         return NULL;
-//       }
-      int moveA = max(0, a - b);
-      int moveB = max(0, b - a);
-      p = headA;
-      q = headB;
-      
-      while(moveA--){
-        p = p->next;
-      }
-      while(moveB--){
-        q = q->next;
-      }
-//       while(p != q){
+//       int a = 1, b = 1;
+//       ListNode *p = headA;
+//       ListNode *q = headB;
+//       while(p->next){
+//         a++;
 //         p = p->next;
+//       }
+//       while(q->next){
+//         b++;
 //         q = q->next;
 //       }
-//       return p;  
-      while(p != nullptr and q != nullptr){
-        if(p == q){
-          return p;
-        } else {
-          p = p->next;
-          q = q->next;
-        }
+// //       if(p != q){
+// //         return NULL;
+// //       }
+//       int moveA = max(0, a - b);
+//       int moveB = max(0, b - a);
+//       p = headA;
+//       q = headB;
+      
+//       while(moveA--){
+//         p = p->next;
+//       }
+//       while(moveB--){
+//         q = q->next;
+//       }
+// //       while(p != q){
+// //         p = p->next;
+// //         q = q->next;
+// //       }
+// //       return p;  
+//       while(p != nullptr and q != nullptr){
+//         if(p == q){
+//           return p;
+//         } else {
+//           p = p->next;
+//           q = q->next;
+//         }
+//       }
+//       return NULL;
+      
+      if(headA == nullptr or headB == nullptr) return nullptr;
+      ListNode *a = headA;
+      ListNode *b = headB;
+      while(a != b){
+        a = (a == nullptr) ? headB : a->next;
+        b = (b == nullptr) ? headA : b->next;
       }
-      return NULL;
+      return a;
     }
 };
