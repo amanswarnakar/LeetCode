d5aad57b623f764fcc5880782feb658fe742fabc
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-07-18 16:39:39 +0530

    Time: 15 ms (60.00%), Space: 8 MB (57.28%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -9,21 +9,24 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-      // if(head == nullptr or head->next == nullptr)  return false;
-      // ListNode *slow = head, *fast = head;
-      // while(fast->next and fast->next->next){
-      //   slow = slow->next;
-      //   fast = fast->next->next;
-      //   if(slow == fast)  return true;
-      // }
-      // return false;
-      
-      unordered_set<ListNode *> s;
-      while(head){
-        if(s.insert(head).second == false)
-          return true;
-        head = head->next;
+      // Two pointer / Floyd Cycle detection
+      // TC - o(n)  SC - o(1)
+      if(head == nullptr or head->next == nullptr)  return false;
+      ListNode *slow = head, *fast = head;
+      while(fast->next and fast->next->next){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast)  return true;
       }
       return false;
+      
+      // Set Approach TC - o(nlogn) SC - O(n)
+      // unordered_set<ListNode *> s;
+      // while(head){
+      //   if(s.insert(head).second == false)
+      //     return true;
+      //   head = head->next;
+      // }
+      // return false;
     }
 };
