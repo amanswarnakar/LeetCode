f5b8121ec386c037061084221b3f9b5b9d7feebd
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-07-22 15:46:58 +0530

    Time: 17 ms (32.82%), Space: 9.5 MB (14.81%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -9,23 +9,38 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-      ListNode *dh = new ListNode(0);
-      dh->next = head;
-      ListNode *fast = dh->next;
-      ListNode *slow = dh->next;
-      if(head == nullptr or head->next == nullptr) return nullptr;
-      while(fast->next and fast->next->next){
-        slow = slow->next;
-        fast = fast->next->next;
-        if(slow == fast){
-          slow = dh->next;
-          while(slow != fast){
-            slow = slow->next;
-            fast = fast->next;
-          }
-          return slow;
-        } 
+//       Flyod cycle detection - Tortoise and Hare(Rabbit) Algo - Two Pointer 
+//       TC - o(n) SC-o(1)
+      
+      // ListNode *dh = new ListNode(0);
+      // dh->next = head;
+      // ListNode *fast = dh->next;
+      // ListNode *slow = dh->next;
+      // if(head == nullptr or head->next == nullptr) return nullptr;
+      // while(fast->next and fast->next->next){
+      //   slow = slow->next;
+      //   fast = fast->next->next;
+      //   if(slow == fast){
+      //     slow = dh->next;
+      //     while(slow != fast){
+      //       slow = slow->next;
+      //       fast = fast->next;
+      //     }
+      //     return slow;
+      //   } 
+      // }
+      // return nullptr;
+      
+      
+//       HashSet
+      
+      unordered_set<ListNode *> s;
+      while(head){
+        if(s.find(head) != s.end()) return head;
+        s.insert(head);
+        head = head->next;
       }
       return nullptr;
+      
     }
 };
