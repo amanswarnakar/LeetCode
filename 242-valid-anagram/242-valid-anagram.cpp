d5886e31501cc4e19846892179cc2e8b03f9388b
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-07-28 22:53:42 +0530

    Time: 34 ms (5.61%), Space: 7.2 MB (77.97%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-      unordered_map<char, int> mp;
+      // HashMap Approach
+      
+      // unordered_map<char, int> mp;
+      // int n = s.length(), m = t.length();
+      // for(int i = 0; i < n; i++){
+      //   mp[s[i]]++;
+      // }
+      // for(int i = 0; i < m; i++){
+      //   if(mp.find(t[i]) != mp.end()){
+      //     mp[t[i]]--;
+      //   } else return false;
+      // }
+      // for(auto p : mp){
+      //   if(p.second != 0){
+      //     return false;
+      //   }
+      // }
+      // return true;
+      
+      
+      // Sorting Approach
+      
       int n = s.length(), m = t.length();
+      if(n != m) return false;
+      sort(s.begin(), s.end());
+      sort(t.begin(), t.end());
       for(int i = 0; i < n; i++){
-        mp[s[i]]++;
-      }
-      for(int i = 0; i < m; i++){
-        if(mp.find(t[i]) != mp.end()){
-          mp[t[i]]--;
-        } else return false;
-      }
-      for(auto p : mp){
-        if(p.second != 0){
+        if(s[i] != t[i])
           return false;
-        }
       }
       return true;
     }
