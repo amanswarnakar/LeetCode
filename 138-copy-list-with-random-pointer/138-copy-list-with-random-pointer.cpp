bce8da11c59285fddb37f3df6008acbf2191774c
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-08-10 10:34:16 +0530

    Time: 15 ms (48.88%), Space: 11.1 MB (88.20%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -19,20 +19,52 @@ public:
     Node* copyRandomList(Node* head) {
       
       // HashMap Solution TC - O(2N)  SC - O(N)
-      map<Node*, Node*> mp;
+      
+      // map<Node*, Node*> mp;
+      // Node *dh = new Node(0);
+      // dh->next = head;
+      // while(head){
+      //   Node *temp = new Node(head->val);
+      //   mp[head] = temp;
+      //   head = head->next;
+      // }
+      // head = dh->next;
+      // while(head){
+      //   mp[head]->next = mp[head->next];
+      //   mp[head]->random = mp[head->random];
+      //   head = head->next;
+      // }
+      // return mp[dh->next];
+      
+      // Pointer Approach   TC - O(2N)  SC - O(1)
+      
       Node *dh = new Node(0);
       dh->next = head;
-      while(head){
-        Node *temp = new Node(head->val);
-        mp[head] = temp;
-        head = head->next;
+      Node *iter = head;
+      Node *front = head;
+      while(iter != NULL){
+        Node *temp = new Node(iter->val);
+        front = iter->next;
+        iter->next = temp;
+        temp->next = front;
+        iter = front;
       }
-      head = dh->next;
-      while(head){
-        mp[head]->next = mp[head->next];
-        mp[head]->random = mp[head->random];
-        head = head->next;
+      iter = dh->next;
+      while(iter != NULL){
+        if(iter->random == NULL) iter->next->random = NULL;
+        else iter->next->random = iter->random->next;
+        iter = iter->next->next;        
       }
-      return mp[dh->next];
+      Node *ans = new Node(0);
+      Node *copy = ans;
+      iter = dh->next;
+      while(iter != NULL){
+        front = iter->next->next;
+        copy->next = iter->next;
+        iter->next = front;
+        iter = front;
+        copy = copy->next;
+      }
+      return ans->next;
     }
 };
