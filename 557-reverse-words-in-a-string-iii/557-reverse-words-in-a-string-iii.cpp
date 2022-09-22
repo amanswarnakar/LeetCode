35b188a390a392ab3a488835949391bba4edd944
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-09-22 23:49:30 +0530

    Time: 14 ms (89.15%), Space: 9.7 MB (75.20%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
       // Two Pointer Approach
-    string reverse(string s){
-      int l = 0, r = s.size() - 1;
-      while(l < r) swap(s[l++], s[r--]);
-      return s;
-    }
+    // string reverse(string s){
+    //   int l = 0, r = s.size() - 1;
+    //   while(l < r) swap(s[l++], s[r--]);
+    //   return s;
+    // }
   
     string reverseWords(string s) {
-      string ans = "";
-      for(int i = 0; i < s.size(); i++){
-        string temp = "";
-        while(i < s.size() and s[i] != ' ') temp += s[i++];
-        ans += reverse(temp);
-        if(i != s.size())
-          ans += ' ';
-        // cout<<temp<<" ~ "<<ans<<"\n";
+      // string ans = "";
+      // for(int i = 0; i < s.size(); i++){
+      //   string temp = "";
+      //   while(i < s.size() and s[i] != ' ') temp += s[i++];
+      //   ans += reverse(temp);
+      //   if(i != s.size())
+      //     ans += ' ';
+      //   // cout<<temp<<" ~ "<<ans<<"\n";
+      // }
+      // return ans;
+      
+      int n = s.size(), j = 0;
+      for(int i = 0; i <= n; i++){
+        if(s[i] == ' ' or s[i] == '\0'){
+          reverse(s.begin() + j, s.begin() + i);
+          j = i + 1;
+        }
       }
-      return ans;
+      return s;
     }
 };
