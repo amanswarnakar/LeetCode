8f6b06bfd32a6fd1477d8a5c08e0da5cc4bf9662
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-10-10 13:30:10 +0530

    Time: 2 ms (51.67%), Space: 6.1 MB (95.35%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     string breakPalindrome(string p) {
-      if(p.size() == 1) return "";
-      for(int i = 0; i < p.size(); i++){
-        if(p[i] != 'a' and i != p.size() / 2){
+      int n = p.size();
+      if(n == 1) return "";
+      for(int i = 0; i < n; i++){
+        if(p[i] != 'a' and i != n / 2){
           p[i] = 'a'; break;
-        } else if(p[i] == 'a' and i == p.size() - 1){
+        } else if(p[i] == 'a' and i == n - 1){
           p[i] = 'b';
         }
       }  
