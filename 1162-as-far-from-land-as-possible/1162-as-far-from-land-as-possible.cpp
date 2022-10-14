d068f1c7f984caed5aa8b6ba37eb6d3bdcc962b5
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-10-14 10:30:01 +0530

    Time: 94 ms (75.61%), Space: 21.4 MB (48.56%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -4,7 +4,8 @@ public:
       if(r < 0 or r >= m or c < 0 or c >= n) return false;
       return true;
     }
-  
+    int dx[4] = {1, -1, 0, 0};
+    int dy[4] = {0, 0, 1, -1};
     int maxDistance(vector<vector<int>>& v) {
       int m = v.size(), n = v[0].size();
       vector<vector<int>> dp(m, vector<int>(n, -1));
@@ -21,22 +22,29 @@ public:
         int r = q.front().first, c = q.front().second;
         int dis = dp[r][c];
         q.pop();
-        if(check(m, n, r+1, c) and dp[r+1][c] == -1){
-          dp[r+1][c] = dis + 1;
-          q.push({r+1, c});
+        for(int i = 0; i < 4; i++){
+          int x = r + dx[i], y = c + dy[i];
+          if(check(m, n, x, y) and dp[x][y] == -1){
+            dp[x][y] = dis + 1;
+            q.push({x, y});
+          }
         }
-        if(check(m, n, r-1, c) and dp[r-1][c] == -1){
-          dp[r-1][c] = dis + 1;
-          q.push({r-1, c});
-        }
-        if(check(m, n, r, c+1) and dp[r][c+1] == -1){
-          dp[r][c+1] = dis + 1;
-          q.push({r, c+1});
-        }
-        if(check(m, n, r, c-1) and dp[r][c-1] == -1){
-          dp[r][c-1] = dis + 1;
-          q.push({r, c-1});
-        }
+        // if(check(m, n, r+1, c) and dp[r+1][c] == -1){
+        //   dp[r+1][c] = dis + 1;
+        //   q.push({r+1, c});
+        // }
+        // if(check(m, n, r-1, c) and dp[r-1][c] == -1){
+        //   dp[r-1][c] = dis + 1;
+        //   q.push({r-1, c});
+        // }
+        // if(check(m, n, r, c+1) and dp[r][c+1] == -1){
+        //   dp[r][c+1] = dis + 1;
+        //   q.push({r, c+1});
+        // }
+        // if(check(m, n, r, c-1) and dp[r][c-1] == -1){
+        //   dp[r][c-1] = dis + 1;
+        //   q.push({r, c-1});
+        // }
       }
       int ans = -1;
       for(int i = 0; i < m; i++){
