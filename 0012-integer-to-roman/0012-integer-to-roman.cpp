bd9f84157663952736989ae50f078fe26f3c9394
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-10-21 00:01:56 +0530

    Time: 30 ms (19.02%), Space: 8.8 MB (27.52%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -4,15 +4,12 @@ public:
       vector<int> val = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
       vector<string> ch = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
       string ans = "";
-      // while(num){
-        for(int i = 0; i < val.size(); i++){
-          while(num >= val[i]){
-            ans += ch[i];
-            num -= val[i];
-            // cout<<num<<" ";
-          }
+      for(int i = 0; i < val.size(); i++){
+        while(num >= val[i]){
+          ans += ch[i];
+          num -= val[i];
         }
-      // }
+      }
       return ans;
     }
 };
