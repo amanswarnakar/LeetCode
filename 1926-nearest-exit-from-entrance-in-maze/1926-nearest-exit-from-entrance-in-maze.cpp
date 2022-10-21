1d2689bebc4039b963408a450dfec0e1741dda6b
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-10-21 15:34:15 +0530

    Time: 181 ms (79.51%), Space: 30.5 MB (62.99%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -15,24 +15,25 @@ public:
       int m = v.size(), n = v[0].size(), sr = ent[0], sc = ent[1];
       int ans = 0;
       if(isBorder(m, n, sr, sc)) v[sr][sc] = '+';
-      queue<pair<int, int>> q;
-      q.push({sr, sc});
+      queue<pair<pair<int, int>, int>> q;
+      q.push({{sr, sc}, 0});
       
       while(!q.empty()){
-        int s = q.size();
-        for(int i = 0; i < s; i++){
-          int r = q.front().first, c = q.front().second;
+        // int s = q.size();
+        // for(int i = 0; i < s; i++){
+          int r = q.front().first.first, c = q.front().first.second;
+          ans = q.front().second;
           q.pop();
           if(isBorder(m, n, r, c) and v[r][c] != '+') return ans;
           for(int i = 0; i < 4; i++){
             int x = r + dx[i], y = c + dy[i];
             if(isValid(m, n, x, y) and v[x][y] == '.'){
-              q.push({x, y});
+              q.push({{x, y}, ans + 1});
               v[x][y] = '*';
             }
           }
-        }
-        ans++;
+        // }
+        // ans++;
       }
       return -1;
     }
