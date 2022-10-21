09af41ad7a4aa946566292d2a2cf2fb934d92155
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-10-21 15:35:47 +0530

    Time: 276 ms (52.50%), Space: 30.5 MB (59.97%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -19,22 +19,43 @@ public:
       q.push({{sr, sc}, 0});
       
       while(!q.empty()){
-        // int s = q.size();
-        // for(int i = 0; i < s; i++){
-          int r = q.front().first.first, c = q.front().first.second;
-          ans = q.front().second;
-          q.pop();
-          if(isBorder(m, n, r, c) and v[r][c] != '+') return ans;
-          for(int i = 0; i < 4; i++){
-            int x = r + dx[i], y = c + dy[i];
-            if(isValid(m, n, x, y) and v[x][y] == '.'){
-              q.push({{x, y}, ans + 1});
-              v[x][y] = '*';
-            }
+        int r = q.front().first.first, c = q.front().first.second;
+        ans = q.front().second;
+        q.pop();
+        if(isBorder(m, n, r, c) and v[r][c] != '+') return ans;
+        for(int i = 0; i < 4; i++){
+          int x = r + dx[i], y = c + dy[i];
+          if(isValid(m, n, x, y) and v[x][y] == '.'){
+            q.push({{x, y}, ans + 1});
+            v[x][y] = '*';
           }
-        // }
-        // ans++;
+        }
       }
       return -1;
+      
+      
+//       int m = v.size(), n = v[0].size(), sr = ent[0], sc = ent[1];
+//       int ans = 0;
+//       if(isBorder(m, n, sr, sc)) v[sr][sc] = '+';
+//       queue<pair<int, int>> q;
+//       q.push({sr, sc});
+      
+//       while(!q.empty()){
+//         int s = q.size();
+//         for(int i = 0; i < s; i++){
+//           int r = q.front().first, c = q.front().second;
+//           q.pop();
+//           if(isBorder(m, n, r, c) and v[r][c] != '+') return ans;
+//           for(int i = 0; i < 4; i++){
+//             int x = r + dx[i], y = c + dy[i];
+//             if(isValid(m, n, x, y) and v[x][y] == '.'){
+//               q.push({x, y});
+//               v[x][y] = '*';
+//             }
+//           }
+//         }
+//         ans++;
+//       }
+//       return -1;
     }
 };
