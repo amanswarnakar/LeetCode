208377eac2de559aacfedd53276a10f7302d0383
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-10-23 10:13:49 +0530

    Time: 95 ms (36.50%), Space: 22.3 MB (44.96%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-      int n = nums.size();
-      vector<int> v(n + 1, 0), ans;
+      int n = nums.size(), x, y;
+      vector<int> v(n + 1, 0);
       for(int i = 0; i < n; i++){
         v[nums[i]]++;
       }
       for(int i = 1; i < n + 1; i++){
-        if(v[i] == 2) ans.push_back(i);
+        if(v[i] == 2) x = i;
+        else if(v[i] == 0) y = i;
       }
-      for(int i = 1; i < n + 1; i++){
-        if(v[i] == 0) ans.push_back(i);
-      }
-      return ans;
+      return {x, y};
     }
 };
