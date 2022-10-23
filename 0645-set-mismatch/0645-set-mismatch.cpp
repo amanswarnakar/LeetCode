c4fa90728bb69c98b17d1e075f54e82aa6fd03ac
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-10-23 10:18:51 +0530

    Time: 161 ms (17.04%), Space: 30.9 MB (30.01%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
+      // Hashmap Approach
+      
       int n = nums.size(), x, y;
-      vector<int> v(n + 1, 0);
+      unordered_map<int, int> um;
       for(int i = 0; i < n; i++){
-        v[nums[i]]++;
+        um[nums[i]]++;
       }
-      for(int i = 1; i < n + 1; i++){
-        if(v[i] == 2) x = i;
-        else if(v[i] == 0) y = i;
+      for(int i = 1; i <= n; i++){
+        if(um.find(i) != um.end()){
+          if(um[i] > 1) x = i;
+        } else y = i;
       }
       return {x, y};
+      
+      // Brute Force
+      
+      // int n = nums.size(), x, y;
+      // vector<int> v(n + 1, 0);
+      // for(int i = 0; i < n; i++){
+      //   v[nums[i]]++;
+      // }
+      // for(int i = 1; i < n + 1; i++){
+      //   if(v[i] == 2) x = i;
+      //   else if(v[i] == 0) y = i;
+      // }
+      // return {x, y};
     }
 };
