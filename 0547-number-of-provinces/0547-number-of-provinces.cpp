7e3628208792a425fa5f8d5b5bf51c668e71a8c0
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-10-24 11:41:31 +0530

    Time: 20 ms (98.06%), Space: 13.8 MB (54.77%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>> &v, vector<bool> &vis, int r){
+      // if(vis[r]) return;
+      vis[r] = true;
+      for(int i = 0; i < v.size(); i++){
+        if(v[r][i] == 1 and !vis[i])
+          dfs(v, vis, i);
+      }
+    }
+  
+    int findCircleNum(vector<vector<int>>& v) {
+      int ans = 0, n = v.size();
+      vector<bool> vis(n, false);
+      for(int i = 0; i < n; i++){
+        if(!vis[i]){
+          ans++; dfs(v, vis, i);
+        }
+      }
+      return ans;
+    }
+  
+  
+    // Wrong Approach
+  
+//     int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+  
+//     void dfs(vector<vector<int>> &v, vector<vector<int>> &vis, int r, int c){
+//       if(min(r, c) < 0 or r >= v.size() or c >= v[0].size() or vis[r][c] == 1 or v[r][c] == 0) return;
+//       vis[r][c] = 1;
+//       for(int i = 0; i < 4; i++)
+//         dfs(v, vis, r + dx[i], c + dy[i]);
+//     }
+  
+//     int findCircleNum(vector<vector<int>>& v) {
+//       int m = v.size(), n = v[0].size(), ans = 0;
+//       vector<vector<int>> vis(m, vector<int>(n, 0));
+//       for(int i = 0; i < m; i++){
+//         for(int j = 0; j < n; j++){
+//           if(v[i][j] == 1 and vis[i][j] == 0)
+//             dfs(v, vis, i, j), ans++;
+//         }
+//       }
+//       return ans;
+//     }
+};
