72f527d3917e510b2670d276eb83dc262fe3e05c
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-10-25 10:35:34 +0530

    Time: 847 ms (25.87%), Space: 126.9 MB (42.47%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,20 +1,44 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>> &graph, int curManager, vector<int> &time){
-      int temp = 0;
-      for(int u : graph[curManager]){
-        temp = max(temp, dfs(graph, u, time));
-      }
-      return time[curManager] + temp;
-    }
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& time) {
-      // DFS
-      
+      // BFS
+
+      int ans = 0;
       vector<vector<int>> graph(n);
+      queue<pair<int, int>> q;
       for(int i = 0; i < n; i++){
-        if(manager[i] != -1)
-          graph[manager[i]].push_back(i);
+        if(manager[i] != -1) graph[manager[i]].push_back(i);
       }
-      return dfs(graph, headID, time);
+      q.push({0, headID});
+      while(!q.empty()){
+        int currTime = q.front().first, currNode = q.front().second;
+        q.pop();
+        for(int node : graph[currNode]){
+          q.push({currTime + time[currNode], node});
+        }
+        ans = max(ans, currTime);
+      }
+      return ans;
     }
+  
+  
+  // DFS
+  
+//     int dfs(vector<vector<int>> &graph, int curManager, vector<int> &time){
+//       int temp = 0;
+//       for(int u : graph[curManager]){
+//         temp = max(temp, dfs(graph, u, time));
+//       }
+//       return time[curManager] + temp;
+//     }
+//     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& time) {
+//       // DFS
+      
+//       vector<vector<int>> graph(n);
+//       for(int i = 0; i < n; i++){
+//         if(manager[i] != -1)
+//           graph[manager[i]].push_back(i);
+//       }
+//       return dfs(graph, headID, time);
+//     }
 };
