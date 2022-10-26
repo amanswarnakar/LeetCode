3000db255a9159d81e82d50faac9204d2fa69d49
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-10-26 23:03:36 +0530

    Time: 754 ms (57.65%), Space: 105.6 MB (80.04%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,62 +1,63 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &graph, vector<vector<int>> &back, vector<bool> &vis, int &ans, int cn){
-      for(auto &node : graph[cn]){
-        if(!vis[node]){
-          vis[node] = true;
-          ans++;
-          dfs(graph, back, vis, ans, node);
-        }
-      }
-      for(auto &node : back[cn]){
-        if(!vis[node]){
-          vis[node] = true;
-          dfs(graph, back, vis, ans, node);          
-        }
-      }
-    }
-  
-    int minReorder(int n, vector<vector<int>>& connections) {
-      vector<bool> vis(n, false);
-      vis[0] = true;
-      vector<vector<int>> graph(n), back(n);
-      for(auto &con: connections){
-        graph[con[0]].emplace_back(con[1]);
-        back[con[1]].emplace_back(con[0]);
-      }
-      int ans = 0;
-      dfs(graph, back, vis, ans, 0);
-      return ans;
-    }
+//     void dfs(vector<vector<int>> &graph, vector<vector<int>> &back, vector<bool> &vis, int &ans, int cn){
+//       for(auto &node : graph[cn]){
+//         if(!vis[node]){
+//           vis[node] = true;
+//           ans++;
+//           dfs(graph, back, vis, ans, node);
+//         }
+//       }
+//       for(auto &node : back[cn]){
+//         if(!vis[node]){
+//           vis[node] = true;
+//           dfs(graph, back, vis, ans, node);          
+//         }
+//       }
+//     }
   
 //     int minReorder(int n, vector<vector<int>>& connections) {
-//       // BFS
-//       int ans = 0;
+//       vector<bool> vis(n, false);
+//       vis[0] = true;
 //       vector<vector<int>> graph(n), back(n);
-//       vector<bool> vis(n);
-//       queue<int> q;
-//       q.emplace(0);
-
-//       for(auto con : connections){
+//       for(auto &con: connections){
 //         graph[con[0]].emplace_back(con[1]);
 //         back[con[1]].emplace_back(con[0]);
 //       }
-
-//       while(!q.empty()){
-//         int curr = q.front();
-//         q.pop();
-//         vis[curr] = true;
-//         for(auto &node: graph[curr]){
-//           if(!vis[node]){
-//             ans++; q.emplace(node);
-//           }
-//         }
-//         for(auto &node: back[curr]){
-//           if(!vis[node]) q.emplace(node);
-//         }
-//       }
+//       int ans = 0;
+//       dfs(graph, back, vis, ans, 0);
 //       return ans;
 //     }
+  
+  
+    int minReorder(int n, vector<vector<int>>& connections) {
+      // BFS
+      int ans = 0;
+      vector<vector<int>> graph(n), back(n);
+      vector<bool> vis(n);
+      queue<int> q;
+      q.emplace(0);
+
+      for(auto &con : connections){
+        graph[con[0]].emplace_back(con[1]);
+        back[con[1]].emplace_back(con[0]);
+      }
+
+      while(!q.empty()){
+        int curr = q.front();
+        q.pop();
+        vis[curr] = true;
+        for(auto &node: graph[curr]){
+          if(!vis[node]){
+            ans++; q.emplace(node);
+          }
+        }
+        for(auto &node: back[curr]){
+          if(!vis[node]) q.emplace(node);
+        }
+      }
+      return ans;
+    }
   
     //  void dfs(vector<vector<int>> &graph, vector<bool> &vis, vector<vector<int>> &outgoing, int cn, int parentNode, int &ans){
     //   vis[cn] = true;
