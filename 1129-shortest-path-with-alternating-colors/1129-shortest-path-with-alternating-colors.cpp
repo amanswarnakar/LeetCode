1a53173bef144f034af7d45afa71e3e73ef5a813
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-10-26 11:54:19 +0530

    Time: 44 ms (44.57%), Space: 15.4 MB (44.15%) - LeetHub

---
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& r, vector<vector<int>>& b) {
+      vector<vector<pair<int, int>>> graph(n);
+      // red edges are marked as 0, blue edges as 1
+      for(auto &edge : r)
+        graph[edge[0]].push_back({edge[1], 0});
+      for(auto &edge : b)
+        graph[edge[0]].push_back({edge[1], 1});
+      vector<int> ans(n, -1);
+      queue<vector<int>> q;   // queue will store node, distance, color
+      q.push({0, 0, -1});  // color is -1 for node 0 only
+      while(!q.empty()){
+        auto front = q.front();
+        int currNode = front[0], dist = front[1], color = front[2];
+        q.pop();
+        if(ans[currNode] == -1)  ans[currNode] = dist;
+        for(auto &node : graph[currNode]){
+          if(node.first != -1 and node.second != color){
+            q.push({node.first, dist + 1, node.second});
+            node.first = -1;
+          }
+        }
+      }
+      return ans;
+    }
+};
