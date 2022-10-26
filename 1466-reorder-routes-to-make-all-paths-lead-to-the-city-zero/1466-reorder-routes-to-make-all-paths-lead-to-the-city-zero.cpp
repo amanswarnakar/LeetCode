ff80a3e85f65195cf7c59a80d89c7549153ba5a9
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-10-26 22:51:45 +0530

    Time: 914 ms (32.29%), Space: 115.3 MB (53.32%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,9 +1,38 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &graph, vector<bool> &vis, vector<vector<int>> &outgoing, int cn, int caller, int &ans){
+    int minReorder(int n, vector<vector<int>>& connections) {
+      // BFS
+      int ans = 0;
+      vector<vector<int>> graph(n), back(n);
+      vector<bool> vis(n);
+      queue<int> q;
+      q.emplace(0);
+      
+      for(auto con : connections){
+        graph[con[0]].emplace_back(con[1]);
+        back[con[1]].emplace_back(con[0]);
+      }
+      
+      while(!q.empty()){
+        int curr = q.front();
+        q.pop();
+        vis[curr] = true;
+        for(auto &node: graph[curr]){
+          if(!vis[node]){
+            ans++; q.emplace(node);
+          }
+        }
+        for(auto &node: back[curr]){
+          if(!vis[node]) q.emplace(node);
+        }
+      }
+      return ans;
+    }
+  
+  /*    void dfs(vector<vector<int>> &graph, vector<bool> &vis, vector<vector<int>> &outgoing, int cn, int parentNode, int &ans){
       vis[cn] = true;
-      if(caller != -1){
-        if(find(outgoing[cn].begin(), outgoing[cn].end(), caller) == outgoing[cn].end())
+      if(parentNode != -1){
+        if(find(outgoing[cn].begin(), outgoing[cn].end(), parentNode) == outgoing[cn].end())
           ans++;
       }
       for(auto &node : graph[cn]){
@@ -23,5 +52,5 @@ public:
       }
       dfs(graph, vis, outgoing, 0, -1, ans);
       return ans;
-    }
+    }  */
 };
