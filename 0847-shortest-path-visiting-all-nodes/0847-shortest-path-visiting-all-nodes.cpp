535ab3aadc4b838a85fe0374fa337a496736c793
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-10-27 11:56:25 +0530

    Time: 228 ms (30.05%), Space: 18.2 MB (40.07%) - LeetHub

--- 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.cpp
+++ 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.cpp
@@ -8,8 +8,8 @@ public:
       set<pair<int, int>> vis;
       for(int i = 0; i < n; i++){
         int mask = 1 << i;
-        // q.emplace(pair<int, pair<int, int>>{i, pair<int, int>{0, mask}});
-        q.push({i, {0, mask}});
+        q.emplace(pair<int, pair<int, int>>{i, pair<int, int>{0, mask}});
+        // q.push({i, {0, mask}});
         vis.insert({i, mask});
       }
       while(!q.empty()){
@@ -20,8 +20,8 @@ public:
           int newMask = mask | (1 << node);
           if(newMask == all) return dist + 1;
           if(vis.find({node, newMask}) == vis.end()){
-            // q.emplace(pair<int, pair<int, int>>{node, pair<int, int>{dist + 1, newMask}});
-            q.push({node, {dist + 1, newMask}});
+            q.emplace(pair<int, pair<int, int>>{node, pair<int, int>{dist + 1, newMask}});
+            // q.push({node, {dist + 1, newMask}});
             vis.insert({node, newMask});
           }
         }
