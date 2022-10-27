63b91aeacf5a0e5626f82cf40db05ae385677f55
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-10-27 15:38:22 +0530

    Time: 20 ms (74.34%), Space: 10.5 MB (98.62%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &graph, vector<vector<int>> &ans, vector<int> &temp, int cn, int n){
-      temp.emplace_back(cn);
-      if(cn == n) ans.emplace_back(temp);
-      for(auto &node: graph[cn]){
+    void dfs(vector<vector<int>> &graph, vector<vector<int>> &ans, vector<int> &temp, int currNode, int n){
+      temp.emplace_back(currNode);
+      if(currNode == n) ans.emplace_back(temp);
+      else for(auto &node: graph[currNode]){
         dfs(graph, ans, temp, node, n);
       }
       temp.pop_back();
