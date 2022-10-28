ae575cd352863c84f5db1b7a69e270bbffc0d20d
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-10-28 09:47:54 +0530

    Time: 64 ms (22.11%), Space: 14.6 MB (32.60%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -2,44 +2,30 @@ class Solution {
 public:
     void dfs(vector<vector<int>> &graph, vector<bool> &vis, int currNode){
       vis[currNode] = true;
-      for(int i = 0; i < graph.size(); i++){
-        if(graph[currNode][i] == 1 and !vis[i])
-          dfs(graph, vis, i);
+      for(auto &node: graph[currNode]){
+        if(!vis[node])
+          dfs(graph, vis, node);
       }
     }
-  
-    int findCircleNum(vector<vector<int>>& v) {
-      int ans = 0, n = v.size();
+    int findCircleNum(vector<vector<int>>& con) {
+      int n = con.size();
+      vector<vector<int>> graph(n);
       vector<bool> vis(n, false);
       for(int i = 0; i < n; i++){
+        for(int j = i; j < n; j++){
+          if(con[i][j]){
+            graph[i].emplace_back(j);
+            graph[j].emplace_back(i);
+          }
+        }
+      }
+      int ans = 0;
+      for(int i = 0; i < n; i++){
         if(!vis[i]){
-          ans++; dfs(v, vis, i);
+          dfs(graph, vis, i); 
+          ans++;
         }
       }
       return ans;
     }
-  
-  
-    // Wrong Approach
-  
-//     int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
-  
-//     void dfs(vector<vector<int>> &v, vector<vector<int>> &vis, int r, int c){
-//       if(min(r, c) < 0 or r >= v.size() or c >= v[0].size() or vis[r][c] == 1 or v[r][c] == 0) return;
-//       vis[r][c] = 1;
-//       for(int i = 0; i < 4; i++)
-//         dfs(v, vis, r + dx[i], c + dy[i]);
-//     }
-  
-//     int findCircleNum(vector<vector<int>>& v) {
-//       int m = v.size(), n = v[0].size(), ans = 0;
-//       vector<vector<int>> vis(m, vector<int>(n, 0));
-//       for(int i = 0; i < m; i++){
-//         for(int j = 0; j < n; j++){
-//           if(v[i][j] == 1 and vis[i][j] == 0)
-//             dfs(v, vis, i, j), ans++;
-//         }
-//       }
-//       return ans;
-//     }
 };
