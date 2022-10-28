dc8eb4739a524538b221423be2c6d192819b1768
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-10-28 09:25:29 +0530

    Time: 48 ms (83.39%), Space: 19.5 MB (87.73%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
-    map<char, int> helper(string s){
-      map<char, int> mp;
-      for(char ch: s){
-        mp[ch]++;
+      vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        unordered_map<string, vector<string>> um;
+        for(string &s: strs){
+          string temp = s;
+          sort(temp.begin(), temp.end());
+          um[temp].push_back(s);
+        }
+        for(auto &p: um){
+          ans.push_back(p.second);
+        }
+        return ans;
       }
-      return mp;
-    }
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-      map<map<char, int>, vector<string>> mp;
-      vector<vector<string>> ans;
-      for(string s: strs){
-        mp[helper(s)].push_back(s);
-      }
-      for(auto p: mp){
-        ans.push_back(p.second);
-      }
-      return ans;
-    }
+  
+    // map<char, int> helper(string s){
+    //   map<char, int> mp;
+    //   for(char ch: s){
+    //     mp[ch]++;
+    //   }
+    //   return mp;
+    // }
+    // vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    //   map<map<char, int>, vector<string>> mp;
+    //   vector<vector<string>> ans;
+    //   for(string s: strs){
+    //     mp[helper(s)].push_back(s);
+    //   }
+    //   for(auto p: mp){
+    //     ans.push_back(p.second);
+    //   }
+    //   return ans;
+    // }
 };
