1572233f0610360ef646d6fb2f1dd575b2199cf7
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-10-28 09:58:01 +0530

    Time: 369 ms (16.69%), Space: 56.1 MB (25.47%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -2,26 +2,24 @@ class Solution {
 public:
     void dfs(vector<vector<int>> &graph, vector<bool> &vis, int currNode){
       vis[currNode] = true;
-      for(int node : graph[currNode]){
-        if(!vis[node])  dfs(graph, vis, node);
+      for(auto &node: graph[currNode]){
+        if(!vis[node])
+          dfs(graph, vis, node);
       }
     }
-    int makeConnected(int n, vector<vector<int>>& v) {
-      int m = v.size();
-      if(m < n - 1) return -1;
-      int ans = 0;
+    int makeConnected(int n, vector<vector<int>>& connections) {
+      int m = connections.size(), ans = -1;
+      if(m < n - 1) return ans;
       vector<bool> vis(n, false);
       vector<vector<int>> graph(n);
-      for(auto edge : v){
-        graph[edge[0]].push_back(edge[1]);
-        graph[edge[1]].push_back(edge[0]);
+      for(auto con: connections){
+        graph[con[0]].emplace_back(con[1]);
+        graph[con[1]].emplace_back(con[0]);
       }
       for(int i = 0; i < n; i++){
-        if(!vis[i]){
-          ans++;
-          dfs(graph, vis, i);
-        }
+        if(!vis[i])
+          dfs(graph, vis, i), ans++;
       }
-      return ans - 1;
+      return ans;
     }
 };
