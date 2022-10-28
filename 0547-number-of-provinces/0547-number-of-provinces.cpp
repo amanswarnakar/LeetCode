7a731be93d30a934395c3cd47a89564cd5462425
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-10-28 09:51:47 +0530

    Time: 62 ms (26.36%), Space: 13.8 MB (70.05%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -2,27 +2,33 @@ class Solution {
 public:
     void dfs(vector<vector<int>> &graph, vector<bool> &vis, int currNode){
       vis[currNode] = true;
-      for(auto &node: graph[currNode]){
-        if(!vis[node])
-          dfs(graph, vis, node);
+      int n = graph.size();
+      // for(auto &node: graph[currNode]){
+      //   if(!vis[node])
+      //     dfs(graph, vis, node);
+      // }
+      for(int i = 0; i < n; i++){
+        if(graph[currNode][i] and !vis[i])
+          dfs(graph, vis, i);
       }
     }
     int findCircleNum(vector<vector<int>>& con) {
       int n = con.size();
-      vector<vector<int>> graph(n);
-      vector<bool> vis(n, false);
-      for(int i = 0; i < n; i++){
-        for(int j = i; j < n; j++){
-          if(con[i][j]){
-            graph[i].emplace_back(j);
-            graph[j].emplace_back(i);
-          }
-        }
-      }
       int ans = 0;
+      vector<bool> vis(n, false);
+      // vector<vector<int>> graph(n);
+      // for(int i = 0; i < n; i++){
+      //   for(int j = i; j < n; j++){
+      //     if(con[i][j]){
+      //       graph[i].emplace_back(j);
+      //       graph[j].emplace_back(i);
+      //     }
+      //   }
+      // }
       for(int i = 0; i < n; i++){
         if(!vis[i]){
-          dfs(graph, vis, i); 
+          dfs(con, vis, i);
+          // dfs(graph, vis, i); 
           ans++;
         }
       }
