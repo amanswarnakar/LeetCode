309bea8be8bf39813c06fa10d903fa5cb1d15e4c
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-10-29 11:10:28 +0530

    Time: 1162 ms (6.43%), Space: 171.6 MB (8.43%) - LeetHub

--- 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
@@ -1,17 +1,35 @@
 class Solution {
 public:
+    static bool comp(pair<int, int> &a, pair<int, int> &b){
+      if(a.first > b.first) return true;
+      else if(a.first == b.first) return a.second > b.second;
+      return false;
+    }
+    
     int earliestFullBloom(vector<int>& p, vector<int>& g) {
       int n = p.size(), ans = 0, temp = 0;
-      vector<vector<int>> v(n, vector<int>(2, 0));
+      vector<pair<int, int>> v(n);
       for(int i = 0; i < n; i++){
-        v[i][0] = g[i];
-        v[i][1] = p[i];
+        v.emplace_back(g[i], p[i]);
       }
-      sort(begin(v), end(v), greater<>());
-      for(int i = 0; i < n; i++){
-        temp += v[i][1];
-        ans = max(ans, temp + v[i][0]);
+      sort(begin(v), end(v), comp); // Sorting Descendingly wrt grow time
+      for(auto &p: v){
+        temp += p.second;
+        ans = max(ans, temp + p.first);
       }
       return ans;
+
+      // int n = p.size(), ans = 0, temp = 0;
+      // vector<vector<int>> v(n, vector<int>(2, 0));
+      // for(int i = 0; i < n; i++){
+        // v[i][0] = g[i];
+        // v[i][1] = p[i];
+      // }
+      // sort(begin(v), end(v), greater<>());  
+      // for(int i = 0; i < n; i++){
+      //   temp += v[i][1];
+      //   ans = max(ans, temp + v[i][0]);
+      // }
+      // return ans;
     }
 };
