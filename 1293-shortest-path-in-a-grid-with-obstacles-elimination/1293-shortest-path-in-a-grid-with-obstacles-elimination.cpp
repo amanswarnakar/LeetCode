bc5dc05e85dd2c9e481932e1221aa8e4361a0ff5
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-10-31 17:13:07 +0530

    Time: 27 ms (92.09%), Space: 9.9 MB (88.04%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -4,8 +4,8 @@ public:
       int dx[4] = { -1, 0, 0, 1 }, dy[4] = { 0, -1, 1, 0 };
       int m = grid.size(), n = grid[0].size(), ans = 0;
       vector<vector<int>> remains(m, vector<int>(n, INT_MIN));
-      queue<vector<int>> q;
-      q.emplace(vector<int>{0, 0, k});
+      queue<array<int, 3>> q;
+      q.push({0, 0, k});
       remains[0][0] = k;
       while (!q.empty()) {
         for (int it = q.size(); it > 0; it--) {
@@ -17,7 +17,7 @@ public:
             if(min(x, y) >= 0 and x < m and y < n){
               int remain = top[2] - grid[x][y];
               if(remain >= 0 && remains[x][y] < remain) {
-                q.emplace(vector<int>{x, y, remain});
+                q.push({x, y, remain});
                 remains[x][y] = remain;
               }
             }
