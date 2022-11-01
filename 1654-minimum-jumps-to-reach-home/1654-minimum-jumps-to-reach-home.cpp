708dae8b809dd2255c6c9b078543d578f15a31cd
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-11-01 21:01:47 +0530

    Time: 39 ms (88.08%), Space: 18.8 MB (76.61%) - LeetHub

--- 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
@@ -2,24 +2,25 @@ class Solution {
 public:
     int minimumJumps(vector<int>& forbidden, int a, int b, int x) {
       unordered_map<int, int> um;
-      for(auto &pos: forbidden){
+      for(auto &pos: forbidden){  // Adding all the forbidden positions
         um[pos]++;
       }
-      queue<pair<int, pair<int, bool>>> q;
+      queue<pair<int, pair<int, bool>>> q;  // Queue will store the Position, Number of hops and a bool variable that will denote if the last hop was in forward direction or backward (True: forward, False: backward)
       q.push({0, {0, true}});
       while(!q.empty()){
         auto node = q.front();
         int currPos = node.first, ans = node.second.first;
-        bool canGoBackwards = node.second.second;
+        bool canGoBackwards = node.second.second;  
         q.pop();
         if(currPos == x) return ans;
-        if(um.find(currPos) != um.end()) continue;
+        if(um.find(currPos) != um.end()) continue;  // If the position is already visited we do not add forward and backward from that position
         um[currPos]++;
         int forward = currPos + a, backward = currPos - b;
         if(canGoBackwards and backward >= 0){
           q.push({backward, {ans + 1, false}});
         }
         if(forward <= 2000 + a + b){    // 1 <= a, b, forbidden[i] <= 2000
+          // We can at max come backwards from (2000 + a + b) position to x position
           q.push({forward, {ans + 1, true}});
         }
       }
