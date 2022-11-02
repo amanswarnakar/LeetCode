74a03d95e0fc6d851bd65be5fcd98243ac47ce25
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-11-02 14:27:13 +0530

    Time: 291 ms (8.31%), Space: 51.5 MB (52.58%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-      map<int, int> mp;
-      for(int i = 0; i < nums.size(); i++){
-        mp[nums[i]]++;
-        if(mp[nums[i]] > 1) return true;
+      // map<int, int> mp;
+      // for(int i = 0; i < nums.size(); i++){
+      //   mp[nums[i]]++;
+      //   if(mp[nums[i]] > 1) return true;
+      // }
+      // return false;
+      
+      unordered_set<int> s;
+      for(auto &num: nums){
+        if(!s.insert(num).second) return true;
+        // Set.insert() returns a pair in which 
+        // the second part of the pair is a bool
+        // value that indicates whether the element 
+        // has been inserted into the set or not.
+        // Returns true if inserted, false otherwise.
       }
       return false;
     }
