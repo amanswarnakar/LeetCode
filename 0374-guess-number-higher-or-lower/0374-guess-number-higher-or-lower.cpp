cb97043dc26399c5e38d67e0a0f7d61fb90952de
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-11-16 22:18:38 +0530

    Time: 4 ms (22.56%), Space: 5.8 MB (89.06%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
@@ -15,8 +15,8 @@ public:
         int mid = l + (r - l) / 2;
         int temp = guess(mid);
         if(!temp) return mid;
-        else if(temp > 0) l = mid + 1;
-        else r = mid - 1;
+        else if(temp < 0) r = mid - 1;
+        else l = mid + 1;
       }
       return 0;
     }
