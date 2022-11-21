ebdc97cfa72aab4f5dcaf127a3165d21318ce40b
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-11-21 10:22:20 +0530

    Time: 49 ms (47.49%), Space: 18.1 MB (72.31%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& v) {
+      int row[9][9] = {0}, col[9][9] = {0}, cube[9][9] = {0};
+      for(int i = 0; i < 9; i++){
+        for(int j = 0; j < 9; j++){
+          if(v[i][j] != '.'){
+            int k = i / 3 * 3 + j / 3;
+            int num = v[i][j] - '1';
+            if(row[i][num] or col[j][num] or cube[k][num])
+              return false;
+            row[i][num] = col[j][num] = cube[k][num] = 1;
+          }
+        }
+      }
+      return true;
+    }
+};
