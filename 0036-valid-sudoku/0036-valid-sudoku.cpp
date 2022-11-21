ab1f1cf770dcacec2fcbe843d9dc5f77bc17467d
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-11-21 10:22:49 +0530

    Time: 47 ms (52.96%), Space: 18 MB (89.15%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -4,7 +4,7 @@ public:
       int row[9][9] = {0}, col[9][9] = {0}, cube[9][9] = {0};
       for(int i = 0; i < 9; i++){
         for(int j = 0; j < 9; j++){
-          if(v[i][j] != '.'){
+          if(isdigit(v[i][j])){
             int k = i / 3 * 3 + j / 3;
             int num = v[i][j] - '1';
             if(row[i][num] or col[j][num] or cube[k][num])
