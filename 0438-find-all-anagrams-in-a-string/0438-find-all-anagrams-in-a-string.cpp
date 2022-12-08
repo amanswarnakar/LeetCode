9ee54a949ee3adb0f4df0ff0aad82f0d823a4d43
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-12-08 13:40:20 +0530

    Time: 11 ms (96.33%), Space: 8.8 MB (39.38%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,25 +1,6 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-      /*
-       if (s1.size() > s2.size()) return {};
-        vector<int> s1v (26, 0), ans;
-        for (auto c : s1) s1v[c - 'a']++;
-        vector<int> s2v (26, 0);
-        int l = 0, r = 0;
-        while (r < s2.size()) {
-            s2v[s2[r]-'a']++;
-            if (r - l + 1 == s1.size()) 
-                if (s1v == s2v) ans.emplace_back(l);
-            if (r - l + 1 < s1.size()) r++;
-            else {
-                s2v[s2[l]-'a']--;
-                l++;
-                r++;
-            }
-        }
-        return ans;
-      */
       vector<int> v1(26), v2(26), ans;
       for(int i = 0; i < p.size(); i++){
         v1[p[i] - 'a']++;
