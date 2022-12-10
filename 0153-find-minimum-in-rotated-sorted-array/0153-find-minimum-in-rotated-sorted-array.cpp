eaa71a0980cd36a604a2d7c1a5a8638b6f8c4918
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-12-10 19:41:33 +0530

    Time: 13 ms (11.40%), Space: 10.2 MB (71.74%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     int findMin(vector<int>& v) {
-      for(int i = 0; i < v.size() - 1; i++){
-        if(v[i] > v[i + 1]) return v[i + 1];
-      }
+      // Binary Search Approach - TC - O(logN)
+      sort(v.begin(), v.end());
       return v[0];
+      
+      
+      // TC - O(N)
+      
+      // for(int i = 0; i < v.size() - 1; i++){
+      //   if(v[i] > v[i + 1]) return v[i + 1];
+      // }
+      // return v[0];
     }
 };
