540b42da7320a02a6a4c8e2392f981959c3bfd00
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-12-20 13:23:53 +0530

    Time: 134 ms (66.92%), Space: 57 MB (51.70%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
@@ -12,23 +12,32 @@
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val, TreeNode *parentNode = NULL) {
+      // Recursive DFS
+      
       if(!root) return new TreeNode(val);
-      TreeNode *curr = root;
-      while(true){
-        if(val < curr->val){
-          if(curr->left) curr = curr->left;
-          else{
-            curr->left = new TreeNode(val);
-            break;
-          }
-        } else {
-          if(curr->right) curr = curr->right;
-          else{
-            curr->right = new TreeNode(val);
-            break;
-          } 
-        }
-      }
+      if(root->val > val) root->left = insertIntoBST(root->left, val);
+      else root->right = insertIntoBST(root->right, val);
       return root;
+      
+      // Iterative BFS
+      
+      // if(!root) return new TreeNode(val);
+      // TreeNode *curr = root;
+      // while(true){
+      //   if(val < curr->val){
+      //     if(curr->left) curr = curr->left;
+      //     else{
+      //       curr->left = new TreeNode(val);
+      //       break;
+      //     }
+      //   } else {
+      //     if(curr->right) curr = curr->right;
+      //     else{
+      //       curr->right = new TreeNode(val);
+      //       break;
+      //     } 
+      //   }
+      // }
+      // return root;
     }
 };
