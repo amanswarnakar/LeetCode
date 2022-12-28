74e0c18bce9444771213db9ba582a266727c398a
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-12-28 12:36:03 +0530

    Time: 5 ms (59.06%), Space: 6.7 MB (51.28%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+      vector<int> freq(58, 0);
+      for(auto &ch: s){
+        freq[ch - 'A']++;
+      }
+      int ans = 0, cnt = 0;
+      for(auto &i: freq){
+        if(i % 2) cnt++;
+      }
+      if(cnt) return accumulate(freq.begin(), freq.end(), 0) - cnt + 1;
+      return accumulate(freq.begin(), freq.end(), 0);
+    }
+};
