fbf45cc7378da185abcbc92060dbc955543454ae
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-12-31 14:49:19 +0530

    Time: 4 ms (67.38%), Space: 7.8 MB (48.56%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -21,7 +21,7 @@ public:
       for(int i = m - 2; i >= 0; i--){
         for(int j = n - 2; j >= 0; j--){
           dp[i][j] = dp[i + 1][j] * v[i + 1][j] + dp[i][j + 1] * v[i][j + 1];
-          // dp[i][j] *= v[i][j];
+          dp[i][j] *= v[i][j];
         }
       }
       return dp[0][0];
