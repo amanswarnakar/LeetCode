adeaca4445574e3172c807320db4b4080cbe822f
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-01-02 14:25:52 +0530

    Time: 1029 ms (29.55%), Space: 0B (100.00%) - LeetHub

---
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select user_id, 
+concat(upper(substr(name, 1, 1)), lower(substr(name, 2, length(name)))) as name 
+from users 
+order by user_id;
+
+# SUBSTR(string_name, start_index, end_index)
