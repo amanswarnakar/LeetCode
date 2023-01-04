57d22fb5ee96fcdf3d36f50c5291ff8d0533b05d
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-01-04 16:30:00 +0530

    Time: 235 ms (84.30%), Space: 103.7 MB (72.00%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -8,10 +8,8 @@ public:
       int ans = 0;
       for(auto &[f, s]: um){
         if(s < 2) return -1;
-        else{
-          if(s % 3 == 0) ans += s / 3;
-          else ans += s / 3 + 1;
-        }
+        else if(s % 3 == 0) ans += s / 3;
+        else ans += s / 3 + 1;
       }
       return ans;
     }
