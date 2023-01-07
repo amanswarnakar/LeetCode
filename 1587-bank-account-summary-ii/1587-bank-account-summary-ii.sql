0a49b96fad5417de9e1a65f7c440267ffbcf365c
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-01-07 16:13:07 +0530

    Time: 936 ms (38.23%), Space: 0B (100.00%) - LeetHub

--- 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -1,13 +1,16 @@
 # Write your MySQL query statement below
-# select name 
-# from users 
-# where account
-select users.name, a.balance
-from users join (
-  select account, balance 
-  from (
-    select account, sum(amount) as balance
-    from transactions
-    group by account
-  ) t where balance > 10000
-) a on users.account = a.account;
+# select users.name, a.balance
+# from users join (
+#   select account, balance 
+#   from (
+#     select account, sum(amount) as balance
+#     from transactions
+#     group by account
+#   ) t where balance > 10000
+# ) a on users.account = a.account;
+
+select u.name as name, sum(t.amount) as balance
+from users u join transactions t
+on u.account = t.account
+group by u.account
+having balance > 10000;
