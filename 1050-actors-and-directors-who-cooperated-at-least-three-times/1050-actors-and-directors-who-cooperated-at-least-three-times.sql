7a5c454dd8b7a5321672058891be078f09afb386
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-01-07 15:57:16 +0530

    Time: 583 ms (31.37%), Space: 0B (100.00%) - LeetHub

--- 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,8 +1,13 @@
 # Write your MySQL query statement below
-select actor_id, director_id 
-from (
-  select actor_id, director_id, count(timestamp) as coop
-  from actordirector
-  group by actor_id, director_id
-) t
-where coop >= 3;
+# select actor_id, director_id 
+# from (
+#   select actor_id, director_id, count(timestamp) as coop
+#   from actordirector
+#   group by actor_id, director_id
+# ) t
+# where coop >= 3;
+
+select actor_id, director_id
+from actordirector
+group by actor_id, director_id
+having count(timestamp) >= 3;
