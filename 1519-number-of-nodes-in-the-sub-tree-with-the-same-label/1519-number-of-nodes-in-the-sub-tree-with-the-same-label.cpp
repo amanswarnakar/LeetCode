084cfd32e35b4201b25deebbe7440f148ffcc7ef
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-01-12 10:55:59 +0530

    Time: 611 ms (81.48%), Space: 219.7 MB (44.45%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -1,11 +1,10 @@
 class Solution {
-    vector<int> ans;
 public:
-    vector<int> dfs(vector<vector<int>> &graph, int prev, int node, string &labels){
+    vector<int> dfs(vector<vector<int>> &graph, vector<int> &ans, int prev, int node, string &labels){
       vector<int> res(26, 0);
       for(auto &adj: graph[node]){
         if(prev != adj){
-          vector<int> temp = dfs(graph, node, adj, labels);
+          vector<int> temp = dfs(graph, ans, node, adj, labels);
           for(int i = 0; i < 26; i++)
             res[i] += temp[i];
         }
@@ -14,13 +13,13 @@ public:
       return res;
     }
     vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
-      ans.resize(n);
+      vector<int> ans(n, 0);
       vector<vector<int>> graph(n);
       for(auto &e: edges){
         graph[e[0]].emplace_back(e[1]);
         graph[e[1]].emplace_back(e[0]);
       }
-      dfs(graph, -1, 0, labels);
+      dfs(graph, ans, -1, 0, labels);
       return ans;
     }
 };
