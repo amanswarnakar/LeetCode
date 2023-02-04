7066e83ab90004f5a892b2b9cfaab695967b586c
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-02-04 14:35:40 +0530

    Time: 10 ms (44.67%), Space: 13.6 MB (83.73%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -5,6 +5,6 @@ public:
       for(int i = 2; i < n; i++){
         cost[i] += min(cost[i - 1], cost[i - 2]);
       }
-      return min(cost[n - 2], cost[n - 1]);
+      return min(cost[n - 1], cost[n - 2]);
     }
 };
