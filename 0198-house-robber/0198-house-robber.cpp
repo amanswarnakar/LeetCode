3d5a0fdd8e87846d3b9633817ea80554c967149f
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-02-07 17:11:58 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (53.26%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    int helper(vector<int> &nums, vector<int> &dp, int idx){
-      if(idx == 0) return nums[0];
-      if(idx < 0) return 0;
+//     int helper(vector<int> &nums, vector<int> &dp, int idx){
+//       if(idx == 0) return nums[0];
+//       if(idx < 0) return 0;
       
-      if(dp[idx] != -1) return dp[idx];
+//       if(dp[idx] != -1) return dp[idx];
       
-      int pick = nums[idx] + helper(nums, dp, idx - 2);
-      int notPick = 0 + helper(nums, dp, idx - 1);
+//       int pick = nums[idx] + helper(nums, dp, idx - 2);
+//       int notPick = 0 + helper(nums, dp, idx - 1);
       
-      return dp[idx] = max(pick, notPick);
-    }
+//       return dp[idx] = max(pick, notPick);
+//     }
     int rob(vector<int>& nums) {
       // Memoization
       
       // int n = nums.size();
       // vector<int> dp(n, -1);
       // return helper(nums, dp, n - 1);
+      
+      // Tabulation
       
       int n = nums.size();
       vector<int> dp(n, nums[0]);
