fcf931117d2e826ee7ea4de2ff8a83be8e73b475
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-02-18 10:42:56 +0530

    Time: 14 ms (25.22%), Space: 18.8 MB (93.14%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -15,9 +15,9 @@ public:
       // DFS
       
       if(!root) return 0;
-      int left = maxDepth(root->left);
-      int right = maxDepth(root->right);
-      return max(left, right) + 1;
+      int left = maxDepth(root->left) + 1;
+      int right = maxDepth(root->right) + 1;
+      return max(left, right);
       
       // BFS
       
