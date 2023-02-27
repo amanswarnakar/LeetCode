00e850dc1e38ac605b0a8b8c91afb782810952b3
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-02-28 02:49:28 +0530

    Time: 60 ms (42.54%), Space: 31.3 MB (85.74%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& v) {
+      // Constant Space Solution
+      
       int n = v.size();
-      vector<int> arr(n + 1, -1);
       for(int i = 0; i < n; i++){
-        if(v[i] < 0 or v[i] > n)
-          continue;
-        arr[v[i]] = 1;
+        while(v[i] > 0 and v[i] <= n and v[i] != v[v[i] - 1]){
+          swap(v[i], v[v[i] - 1]);
+        }
       }
-      for(int i = 1; i < n + 1; i++){
-        if(arr[i] == -1) return i;
+      for(int i = 0; i < n; i++){
+        if(v[i] != i + 1) return i + 1;
       }
       return n + 1;
+      
+      
+      // Extra Space Solution
+      
+      // int n = v.size();
+      // vector<int> arr(n + 1, -1);
+      // for(int i = 0; i < n; i++){
+      //   if(v[i] < 0 or v[i] > n)
+      //     continue;
+      //   arr[v[i]] = 1;
+      // }
+      // for(int i = 1; i < n + 1; i++){
+      //   if(arr[i] == -1) return i;
+      // }
+      // return n + 1;
     }
 };
