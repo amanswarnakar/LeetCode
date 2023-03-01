f73348b4fba1322eb7644c84395952e335072c27
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-03-01 12:23:10 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (79.49%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    void helper(vector<int> &v, vector<bool> &vis, vector<int> &temp, vector<vector<int>> &ans){
-      if(temp.size() == v.size())
+    void recur(vector<int> &v, vector<bool> &vis, vector<int> &temp, vector<vector<int>> &ans){
+      if(temp.size() == v.size()){
         ans.emplace_back(temp);
+        return;
+      }
       for(int i = 0; i < v.size(); i++){
         if(!vis[i]){
           vis[i] = true;
           temp.emplace_back(v[i]);
-          helper(v, vis, temp, ans);
+          recur(v, vis, temp, ans);
           temp.pop_back();
           vis[i] = false;
         }
       }
     }
-    vector<vector<int>> permute(vector<int>& nums) {
-      int n = nums.size(); 
-      vector<bool> vis(n, false);
+    vector<vector<int>> permute(vector<int>& v) {
+      int n = v.size();
       vector<int> temp;
       vector<vector<int>> ans;
-      helper(nums, vis, temp, ans);
+      vector<bool> vis(n, false);
+      recur(v, vis, temp, ans);
       return ans;
     }
 };
