79fa097b6bf9282a816288aa725ca47d24bd24de
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-03-01 12:36:52 +0530

    Time: 2377 ms (5.01%), Space: 269.7 MB (21.34%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void recur(string s, string temp, vector<bool> &vis, vector<string> &ans, int &cnt, int k){
+      if(temp.size() == s.size()){
+        ans.emplace_back(temp);
+        cnt++;
+        if(cnt == k) return;
+      }
+      for(int i = 0; i < s.size(); i++){
+        if(!vis[i]){
+          vis[i] = true;
+          temp += s[i];
+          recur(s, temp, vis, ans, cnt, k);
+          temp.pop_back();
+          vis[i] = false;
+        }
+        if(cnt == k) return;
+      }
+    }
+    string getPermutation(int n, int k) {
+      vector<string> ans;
+      string s = "", temp = "";
+      for(int i = 1; i < n + 1; i++)
+        s += to_string(i);
+      vector<bool> vis(n, false);
+      int cnt = 0;
+      recur(s, temp, vis, ans, cnt, k);
+      // sort(ans.begin(), ans.end());
+      return ans.back();
+    }
+};
