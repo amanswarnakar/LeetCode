1106922ca7e110636234040fdb9c414ff97b10fd
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-03-11 20:52:28 +0530

    Added solution - LeetHub

---
+++ Maximum Triplet product - GFG/maximum-triplet-product.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+//Initial Template for CPP
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    long long maxTripletProduct(long long v[], int n)
+    {
+    	// Complete the function
+    	sort(v, v + n);
+    	return max(1ll * (v[0] * v[1] * v[n - 1]), 1ll * v[n - 1] * v[n - 2] * v[n - 3]);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,i;
+	    cin>>n; long long arr[n];
+	    for(i=0;i<n;i++)
+	    cin>>arr[i];
+	    Solution ob;
+	    cout <<ob.maxTripletProduct(arr, n);
+	    cout<<"\n";
+	}
+    return 0;
+}
+// } Driver Code Ends
