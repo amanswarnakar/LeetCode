f7329aaae65d3cb2d187b4d2e06c428d34ea355c
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-03-22 13:43:14 +0530

    Time: 539 ms (48.31%), Space: 127.8 MB (65.54%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,5 +1,31 @@
 class Solution {
 public:
+    int minScore(int n, vector<vector<int>>& roads) {
+      vector<vector<pair<int, int>>> graph(n);
+      for(auto &road: roads){
+        graph[road[0] - 1].emplace_back(make_pair(road[1] - 1, road[2]));
+        graph[road[1] - 1].emplace_back(make_pair(road[0] - 1, road[2]));
+      }
+      vector<bool> vis(n, false);
+      int ans = INT_MAX;
+      queue<int> q;
+      q.emplace(0);
+      while(!q.empty()){
+        int sz = q.size();
+        while(sz--){
+          auto node = q.front(); q.pop();
+          vis[node] = true;
+          for(auto &[adj, dist]: graph[node]){
+            ans = min(ans, dist);
+            if(!vis[adj]) q.emplace(adj);
+          }
+        }
+      }
+      return ans;
+    }
+  
+  
+  /*
     void dfs(vector<vector<pair<int, int>>> &graph, int node, vector<bool> &vis, int &ans){
       if(vis[node]) return;
       vis[node] = true;
@@ -19,4 +45,5 @@ public:
       dfs(graph, 0, vis, ans);
       return ans;
     }
+  */
 };
