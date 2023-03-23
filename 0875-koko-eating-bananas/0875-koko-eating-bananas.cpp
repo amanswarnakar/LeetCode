cc1ae06c9d588b8102f0aefb9caa551e559ec78b
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-03-23 21:13:18 +0530

    Time: 49 ms (55.54%), Space: 18.9 MB (51.87%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,5 +1,23 @@
 class Solution {
 public:
+    bool isPossible(vector<int> &v, int mid, int h){
+      long long int res = 0;
+      for(auto &i: v){
+        res += (i / mid) + (i % mid != 0);
+      }
+      return res <= h;
+    }
+    int minEatingSpeed(vector<int>& v, int h) {
+      int l = 1, r = *max_element(begin(v), end(v));
+      while(l <= r){
+        int mid = l + (r - l) / 2;
+        if(isPossible(v, mid, h)) r = mid - 1;
+        else l = mid + 1;
+      }
+      return l;
+    }
+  
+  /*
     long long int helper(vector<int> &v, int b){
       long long int res = 0, n = v.size();
       for(int i = 0; i < n; i++){
@@ -19,4 +37,5 @@ public:
       }
       return l;
     }
+  */
 };
