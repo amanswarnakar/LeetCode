c145f89b509cd48d543fc53bdf7ccd0660efacf1
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-03-23 22:39:41 +0530

    Time: 346 ms (69.00%), Space: 94.6 MB (33.19%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    bool isPossible(vector<int> &v, long long time, int t){
+   bool isPossible(vector<int> &v, long long time, long long t){
       long long res = 0;
-      for(auto i: v){
-        res += time / i;
+      for(int i = 0; i < v.size(); i++){
+        res += time / v[i];
       }
       return res >= t;
     }
     long long minimumTime(vector<int>& v, int t) {
-      long long int l = 1;
-      long long int r = 1LL * *min_element(v.begin(), v.end()) * t;
+      long long int l = 1, r = 1LL * *min_element(begin(v), end(v)) * t;
       while(l < r){
         long long mid = l + (r - l) / 2;
         if(isPossible(v, mid, t)) r = mid;
