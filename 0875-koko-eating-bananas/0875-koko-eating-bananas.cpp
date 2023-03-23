2d6a8de609005910f2527a7fc29609bb0bd74844
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-03-23 21:08:57 +0530

    Time: 67 ms (21.37%), Space: 19 MB (10.83%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
+    long long int helper(vector<int> &v, int b){
+      long long int res = 0, n = v.size();
+      for(int i = 0; i < n; i++){
+        res += (v[i] / b) + (v[i] % b != 0);
+      }
+      return res;
+    }
     int minEatingSpeed(vector<int>& v, int h) {
-      sort(v.begin(), v.end());
-      int n = v.size(), l = 1, r = v[n - 1], ans = 0;
+      // sort(begin(v), end(v));
+      long long int n = v.size(), noOfHours = 0;
+      long long int l = 1, r = *max_element(begin(v), end(v));
       while(l <= r){
         int mid = l + (r - l) / 2;
-        long long int cnt = 0;
-        for(int i = 0; i < n; i++){
-          if(v[i] < mid) cnt++;
-          else cnt += (v[i] - 1) / mid + 1;
-        }
-        if(cnt > h) l = mid + 1;
-        else r = mid - 1;
+        noOfHours = helper(v, mid);
+        if(noOfHours <= h) r = mid - 1;
+        else l = mid + 1;
       }
       return l;
     }
