8b9a15bf034bc04c58f0e81cab298b2fa4fa6683
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-03-25 11:10:35 +0530

    Time: 3 ms (98.92%), Space: 10 MB (81.44%) - LeetHub

--- 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.cpp
+++ 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.cpp
@@ -6,10 +6,6 @@ public:
       for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
           mxx[i] = max(mxx[i], grid[i][j]);
-        }
-      }
-      for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
           mxy[i] = max(mxy[i], grid[j][i]);
         }
       }
