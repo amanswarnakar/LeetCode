696f92286380662cd75943aee0506c08b10be4e5
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-03-28 23:52:01 +0530

    Time: 5 ms (25.17%), Space: 7.5 MB (100.00%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -12,6 +12,20 @@ public:
 //       return dp[idx] = max(pick, notPick);
 //     }
     int rob(vector<int>& nums) {
+      // Constant Space 
+      
+      int n = nums.size();
+      if(n <= 2) return *max_element(begin(nums), end(nums));
+      int prev1 = nums[0], prev2 = nums[1];
+      for(int i = 1; i < n; i++){
+        int pick = nums[i];
+        if(i > 1) pick += prev2;
+        int notPick = 0 + prev1;
+        prev2 = prev1;
+        prev1 = max(pick, notPick);
+      }
+      return prev1;
+      
       // Memoization
       
       // int n = nums.size();
@@ -20,13 +34,13 @@ public:
       
       // Tabulation
       
-      int n = nums.size();
-      vector<int> dp(n, nums[0]);
-      for(int i = 1; i < n; i++){
-        dp[i] = nums[i];
-        if(i > 1) dp[i] += dp[i - 2];
-        dp[i] = max(dp[i], dp[i - 1]);
-      }
-      return dp[n - 1];
+      // int n = nums.size();
+      // vector<int> dp(n, nums[0]);
+      // for(int i = 1; i < n; i++){
+      //   dp[i] = nums[i];
+      //   if(i > 1) dp[i] += dp[i - 2];
+      //   dp[i] = max(dp[i], dp[i - 1]);
+      // }
+      // return dp[n - 1];
     }
 };
