983adba014e48f79f3ac4b8e52c4319404c03170
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-03-29 00:06:51 +0530

    Time: 3 ms (42.77%), Space: 8.2 MB (7.95%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int helper(vector<int> &v){
-      int prev = v[0], prev2 = 0, n = v.size();
-      for(int i = 1; i < n; i++){
-        int pick = v[i];
-        if(i > 1) pick += prev2;
-        int notPick = 0 + prev;
-        int curr = max(pick, notPick);
-        prev2 = prev;
-        prev = curr;
-      }
-      return prev;
+    int recur(int idx, vector<int> &v, int n, vector<int> &dp){
+      if(idx == 0) return v[0];
+      if(idx < 0) return 0;
+      if(dp[idx] != -1) return dp[idx];
+      int pick = v[idx] + recur(idx - 2, v, n, dp);
+      int notPick = 0 + recur(idx - 1, v, n, dp);
+      return dp[idx] = max(pick, notPick);
     }
     int rob(vector<int>& v) {
       int n = v.size();
-      if(n == 1) return v[0];
-      vector<int> temp1, temp2;
+      if(n <= 2) return *max_element(v.begin(), v.end());
+      vector<int> v1, v2, dp(n - 1, -1);
       for(int i = 0; i < n; i++){
-        if(i != 0) temp1.emplace_back(v[i]);
-        if(i != n - 1) temp2.emplace_back(v[i]);
+        if(i != 0) v1.emplace_back(v[i]);
+        if(i != n - 1) v2.emplace_back(v[i]);
       }
-      return max(helper(temp1), helper(temp2));
+      int x = recur(n - 2, v1, n - 1, dp);
+      for(auto &i: dp) i = -1;
+      int y = recur(n - 2, v2, n - 1, dp);
+      return max(x, y);
     }
 };
