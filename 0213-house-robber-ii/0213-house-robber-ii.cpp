c8b908b2c5f2f039260d32a7d0d3d39029c15a37
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-03-29 00:15:04 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (7.95%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -8,6 +8,19 @@ public:
       int notPick = 0 + recur(idx - 1, v, n, dp);
       return dp[idx] = max(pick, notPick);
     }
+    
+    int helper(vector<int> &v){
+      int n = v.size(), p1 = v[0], p2 = v[1];
+      for(int i = 1; i < n; i++){
+        int pick = v[i];
+        if(i > 1) pick += p2;
+        int notPick = 0 + p1;
+        p2 = p1;
+        p1 = max(pick, notPick);
+      }
+      return p1;
+    }
+  
     int rob(vector<int>& v) {
       int n = v.size();
       if(n <= 2) return *max_element(v.begin(), v.end());
@@ -16,9 +29,11 @@ public:
         if(i != 0) v1.emplace_back(v[i]);
         if(i != n - 1) v2.emplace_back(v[i]);
       }
-      int x = recur(n - 2, v1, n - 1, dp);
-      for(auto &i: dp) i = -1;
-      int y = recur(n - 2, v2, n - 1, dp);
-      return max(x, y);
+      return max(helper(v1), helper(v2));
+      
+      // int x = recur(n - 2, v1, n - 1, dp);
+      // dp.clear();
+      // int y = recur(n - 2, v2, n - 1, dp);
+      // return max(x, y);
     }
 };
