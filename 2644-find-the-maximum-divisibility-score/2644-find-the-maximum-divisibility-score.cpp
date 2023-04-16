514e3330b3a62b3d2c202b1c81953b6b514dcd89
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-04-16 11:31:43 +0530

    Time: 1167 ms (33.33%), Space: 29.5 MB (33.33%) - LeetHub

--- 2644-find-the-maximum-divisibility-score/2644-find-the-maximum-divisibility-score.cpp
+++ 2644-find-the-maximum-divisibility-score/2644-find-the-maximum-divisibility-score.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int maxDivScore(vector<int>& v, vector<int>& div) {
       int ans = 0, maxx = 0;
-      sort(div.begin(), div.end(), greater<int>());
-      for(int i = 0; i < div.size(); i++){
+      sort(div.begin(), div.end());
+      for(int i = div.size() - 1; i >= 0; i--){
         int temp = 0;
         for(int j = 0; j < v.size(); j++){
           if(v[j] % div[i] == 0) temp++;
