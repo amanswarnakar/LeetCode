1d18afd95c8729a475434877adcab9f9ab88006d
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-04-24 23:27:40 +0530

    Time: 0 ms (100.00%), Space: 8 MB (22.42%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,5 +1,31 @@
 class Solution {
 public:
+    int solve(vector<int> &v){
+      int n = v.size();
+      vector<int> dp(n, -1);
+      dp[0] = v[0];
+      for(int i = 1; i < n; i++){
+        int pick = v[i];
+        if(i > 1) pick += dp[i - 2];
+        int notPick = dp[i - 1];
+        dp[i] = max(pick, notPick);
+      }
+      return dp[n - 1];
+    }
+  
+    int rob(vector<int>& v) {
+      int n = v.size(), ans = 0;
+      if(n <= 2) return *max_element(v.begin(), v.end());
+      vector<int> v1, v2;
+      // vector<int> dp(n - 1, -1);
+      for(int i = 0; i < n; i++){
+        if(i != n - 1) v1.emplace_back(v[i]);
+        if(i != 0) v2.emplace_back(v[i]);
+      }
+      ans = max(solve(v1), solve(v2));
+      return ans;
+    }
+  /*
     int solve(int idx, vector<int> &v, vector<int> &dp){
       if(idx < 0) return 0;
       if(dp[idx] != -1) return dp[idx];
@@ -20,4 +46,5 @@ public:
       ans = max(ans, solve(n - 2, v2, dp));
       return ans;
     }
+  */
 };
