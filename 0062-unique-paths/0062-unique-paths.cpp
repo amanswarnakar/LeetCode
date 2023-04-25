ac0d5a2ad1ff25f76ad5cdff14509101aab7f40b
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-04-25 23:32:43 +0530

    Time: 4 ms (23.43%), Space: 6.2 MB (69.70%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,5 +1,27 @@
 class Solution {
 public:
+  // Space Optimised Solution
+    int uniquePaths(int m, int n) {
+      vector<int> prev(n, 0);
+      for(int i = 0; i < m; i++){
+        vector<int> curr(n, 0);
+        for(int j = 0; j < n; j++){
+          if(i == 0 and j == 0) curr[j] = 1;
+          else {
+            int top = 0, left = 0;
+            if(i > 0) top = prev[j];
+            if(j > 0) left = curr[j - 1];
+            curr[j] = top + left;
+          }
+        }
+        prev = curr;
+      }
+      return prev[n - 1];
+    }
+  
+  /*
+  // Top Down Approach
+  
     int uniquePaths(int m, int n) {
       vector<vector<int>> dp(m, vector<int>(n, 0));
       for(int i = 0; i < m; i++) dp[i][0] = 1;
@@ -11,7 +33,11 @@ public:
       }
       return dp[m - 1][n - 1];
     }
+  */
+  
   /*
+  // Bottom Up Approach
+  
     int solve(int r, int c, vector<vector<int>> &dp){
       if(r == 0 and c == 0) return 1;
       if(r < 0 or c < 0) return 0;
