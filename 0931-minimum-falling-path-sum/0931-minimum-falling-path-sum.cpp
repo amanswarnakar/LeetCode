e049077cffedf2036c95e203034083e322ca5579
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-04-26 23:59:16 +0530

    Time: 17 ms (43.87%), Space: 10.4 MB (32.87%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,5 +1,27 @@
 class Solution {
 public:
+    // Tabulation
+  
+    int minFallingPathSum(vector<vector<int>>& v) {
+      int m = v.size(), n = v[0].size(), ans = 0;
+      vector<vector<int>> dp(m, vector<int>(n, 0));
+      for(int i = 0; i < n; i++) dp[m - 1][i] = v[m - 1][i];
+      for(int i = m - 2; i >= 0; i--){
+        for(int j = 0; j < n; j++){
+          int left = 1e9, down = dp[i + 1][j], right = 1e9;
+          if(j > 0) left = dp[i + 1][j - 1];
+          if(j < n - 1) right = dp[i + 1][j + 1];
+          dp[i][j] = v[i][j] + min(left, min(down, right));
+        }
+      }
+      // for(int &i: dp[0]) ans = min(ans, i);
+      // return ans;
+      return *min_element(dp[0].begin(), dp[0].end());
+    }
+  
+    /*
+    // Memoization 
+    
     int solve(int r, int c, vector<vector<int>> &g, vector<vector<int>> &dp){
       if(c < 0 or c == g[0].size()) return 1e9;
       if(r == 0) return g[0][c];
@@ -19,4 +41,5 @@ public:
       }
       return ans;
     }
+    */
 };
