c5ea1db52d2f6ffb77c137d24bba9faa1ba3ce1e
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-04-26 22:08:57 +0530

    Time: 4 ms (41.17%), Space: 7.8 MB (71.11%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int solve(int r, int c, vector<vector<int>> &g, vector<vector<int>> &dp){
-      if(r < 0 or c < 0 or g[r][c] == 1) return INT_MIN;
+      if(r < 0 or c < 0 or g[r][c] == 1) return 0;
       if(r == 0 and c == 0) return 1;
       if(dp[r][c] != -1) return dp[r][c];
       int top = solve(r - 1, c, g, dp);
       int left = solve(r, c - 1, g, dp);
-      if(top < 0) return dp[r][c] = left < 0 ? 0 : left;
-      if(left < 0) return dp[r][c] = top < 0 ? 0 : top;
+      // if(top < 0) return dp[r][c] = left < 0 ? 0 : left;
+      // if(left < 0) return dp[r][c] = top < 0 ? 0 : top;
       return dp[r][c] = top + left;
     }
     int uniquePathsWithObstacles(vector<vector<int>>& g) {
       int m = g.size(), n = g[0].size();
       vector dp(m, vector<int>(n, -1));
-      int ans = solve(m - 1, n - 1, g, dp);
-      return ans < 0 ? 0 : ans;
+      return solve(m - 1, n - 1, g, dp);
     }
 };
