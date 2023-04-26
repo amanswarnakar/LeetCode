2e709fa71539bef513b139f8200a2abca7311f11
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-04-26 23:52:09 +0530

    Time: 19 ms (28.78%), Space: 10.1 MB (73.34%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>>& v) {
-      int m = v.size(), n = v[0].size(), ans = INT_MAX;
-      vector<vector<int>> dp(m, vector<int>(n + 2, INT_MAX));
-      for(int i = 1; i < n + 1; i++){
-        dp[m - 1][i] = v[m - 1][i - 1];
+    int solve(int r, int c, vector<vector<int>> &g, vector<vector<int>> &dp){
+      if(c < 0 or c == g[0].size()) return 1e9;
+      if(r == 0) return g[0][c];
+      if(dp[r][c] != -1) return dp[r][c];
+      int top = solve(r - 1, c, g, dp);
+      int topLeft = solve(r - 1, c - 1, g, dp);
+      int topRight = solve(r - 1, c + 1, g, dp);
+      return dp[r][c] = g[r][c] + min(top, min(topLeft, topRight));
+    }
+  
+    int minFallingPathSum(vector<vector<int>>& g) {
+      int m = g.size(), n = g[0].size();
+      int ans = INT_MAX;
+      vector<vector<int>> dp(m, vector<int>(n, -1));
+      for(int i = 0; i < n; i++){
+        ans = min(ans, solve(m - 1, i, g, dp));
       }
-      for(int i = m - 2; i >= 0; i--){
-        for(int j = 1; j < n + 1; j++){
-          dp[i][j] = v[i][j - 1] + min(dp[i + 1][j - 1], min(dp[i + 1][j], dp[i + 1][j + 1]));
-        }
-      }
-      for(auto i: dp[0])
-        ans = min(ans, i);
       return ans;
     }
 };
