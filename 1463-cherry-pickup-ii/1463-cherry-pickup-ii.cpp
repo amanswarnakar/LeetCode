03b20592059be6686eba0c7c709db138244e1128
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-04-27 01:38:12 +0530

    Time: 205 ms (25.46%), Space: 9.2 MB (96.34%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -2,6 +2,42 @@ class Solution {
 public:
     int cherryPickup(vector<vector<int>>& g) {
       int m = g.size(), n = g[0].size();
+      vector<vector<int>> prev(n, vector<int>(n, 0)), curr(n, vector<int>(n, 0));
+      for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+          if(i == j) prev[i][j] = g[m - 1][i];
+          else prev[i][j] = g[m - 1][i] + g[m - 1][j];
+        }
+      }
+      
+      for(int r = m - 2; r >= 0; r--){
+        for(int c1 = 0; c1 < n; c1++){
+          for(int c2 = 0; c2 < n; c2++){
+            
+            int temp = -1e8;
+            for(int i = -1; i < 2; i++){
+              for(int j = -1; j < 2; j++){
+                int val = g[r][c1];
+                if(c1 != c2) val += g[r][c2];
+                if(c1 + i >= 0 and c1 + i < g[0].size() and c2 + j >= 0 and c2 + j < g[0].size())
+                  val += prev[c1 + i][c2 + j];
+                else val = -1e8;
+                temp = max(temp, val);
+              }
+            }
+            curr[c1][c2] = temp;
+          }
+        }
+        prev = curr;
+      }
+      return prev[0][n - 1];
+    }
+  
+    /*
+    // Tabulation 
+    
+    int cherryPickup(vector<vector<int>>& g) {
+      int m = g.size(), n = g[0].size();
       vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(n, 0)));
       for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
@@ -31,6 +67,7 @@ public:
       }
       return dp[0][0][n - 1];
     }
+    */
     
     /*
     // Memoization
