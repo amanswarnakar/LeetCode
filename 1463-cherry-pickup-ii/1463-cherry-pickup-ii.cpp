3dbfb99f4b42707a3433d411bb2987c248903aa5
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-04-27 01:35:03 +0530

    Time: 291 ms (6.15%), Space: 17 MB (26.83%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,5 +1,40 @@
 class Solution {
 public:
+    int cherryPickup(vector<vector<int>>& g) {
+      int m = g.size(), n = g[0].size();
+      vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(n, 0)));
+      for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+          if(i == j) dp[m - 1][i][j] = g[m - 1][i];
+          else dp[m - 1][i][j] = g[m - 1][i] + g[m - 1][j];
+        }
+      }
+      
+      for(int r = m - 2; r >= 0; r--){
+        for(int c1 = 0; c1 < n; c1++){
+          for(int c2 = 0; c2 < n; c2++){
+            
+            int temp = -1e8;
+            for(int i = -1; i < 2; i++){
+              for(int j = -1; j < 2; j++){
+                int val = g[r][c1];
+                if(c1 != c2) val += g[r][c2];
+                if(c1 + i >= 0 and c1 + i < g[0].size() and c2 + j >= 0 and c2 + j < g[0].size())
+                  val += dp[r + 1][c1 + i][c2 + j];
+                else val = -1e8;
+                temp = max(temp, val);
+              }
+            }
+            dp[r][c1][c2] = temp;
+          }
+        }
+      }
+      return dp[0][0][n - 1];
+    }
+    
+    /*
+    // Memoization
+    
     int solve(int c1, int c2, int r, vector<vector<int>> &g, vector<vector<vector<int>>> &dp){
       if(min(c1, c2) < 0 or max(c1, c2) == g[0].size()) return 0;
       if(r == g.size() - 1)
@@ -20,4 +55,5 @@ public:
       int ans = solve(0, n - 1, 0, g, dp);
       return ans;
     }
+    */
 };
