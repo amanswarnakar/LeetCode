1f5b428d319732d6051ac2e7c6f3510029a54bcf
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-04-26 23:04:53 +0530

    Time: 12 ms (12.19%), Space: 9 MB (37.75%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
+    int solve(int r, int idx, vector<vector<int>> &v, vector<vector<int>> &dp){
+      if(r == 0) return v[0][0];
+      if(idx < 0 or idx == v[r].size()) return 1e9;
+      if(dp[r][idx] != -1) return dp[r][idx];
+      int topLeft = 1e9, topRight = 1e9;
+      topLeft = solve(r - 1, idx - 1, v, dp);
+      topRight = solve(r - 1, idx, v, dp);
+      return dp[r][idx] = v[r][idx] + min(topLeft, topRight);
+    }
+  
     int minimumTotal(vector<vector<int>>& v) {
       int n = v.size();
-      for(int i = n - 2; i >= 0; i--){
-        for(int j = i; j >= 0; j--){
-          v[i][j] += min(v[i + 1][j], v[i + 1][j + 1]);
-        }
+      int ans = INT_MAX;
+      vector dp(n, vector<int>(n, -1));
+      for(int i = 0; i < n; i++){
+        int temp = solve(n - 1, i, v, dp);
+        ans = min(ans, temp);
       }
-      return v[0][0];
+      return ans;
     }
 };
