a22e8e6992846671071228120fe73ffaf460b4f8
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-04-29 11:27:32 +0530

    Time: 533 ms (13.40%), Space: 13.2 MB (70.39%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,5 +1,30 @@
 class Solution {
 public:
+    // Tabulation
+  
+    bool canPartition(vector<int>& v) {
+      int n = v.size(), sum = accumulate(v.begin(), v.end(), 0);
+      if((sum & 1) == 1) return false;
+      sum /= 2;
+      vector<vector<bool>> dp(n, vector<bool>(sum + 1, 0));
+      if(v[0] <= sum) dp[0][v[0]] = true;
+      
+      for(int i = 0; i < n; i++) dp[i][0] = true;
+      
+      for(int i = 1; i < n; i++){
+        for(int j = 1; j < sum + 1; j++){
+          bool notPick = dp[i - 1][j];
+          bool pick = false;
+          if(v[i] <= j) pick = dp[i - 1][j - v[i]];
+          dp[i][j] = pick or notPick;
+        }
+      }
+      return dp[n - 1][sum];
+    }
+  
+    /*
+    // Memoization
+    
     bool f(int idx, int t, vector<int> &v, vector<vector<int>> &dp){
       if(t == 0) return true;
       if(idx == 0) return v[0] == t;
@@ -16,4 +41,5 @@ public:
       vector<vector<int>> dp(n, vector<int>(sum + 1, -1));
       return f(n - 1, sum, v, dp);
     }
+    */
 };
