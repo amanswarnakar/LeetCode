36a6a2ca849467fdb3cee4c9101112e977188d5e
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-05-03 15:18:32 +0530

    Time: 178 ms (14.04%), Space: 45 MB (37.82%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,5 +1,24 @@
 class Solution {
 public:
+    int coinChange(vector<int>& v, int t) {
+      int n = v.size();
+      vector<vector<int>> dp(n, vector<int>(t + 1, 0));
+      for(int i = 0; i < t + 1; i++){
+        if(i % v[0] == 0) dp[0][i] = i / v[0];
+        else dp[0][i] = 1e9;
+      }
+      for(int i = 1; i < n; i++){
+        for(int j = 0; j < t + 1; j++){
+          int pick = 1e9;
+          if(v[i] <= j) pick = 1 + dp[i][j - v[i]];
+          int notPick = dp[i - 1][j];
+          dp[i][j] = min(pick, notPick);
+        }
+      }
+      return dp[n - 1][t] >= 1e9 ? -1 : dp[n - 1][t];
+    }
+  
+    /*
     int f(int idx, int t, vector<int> &v, vector<vector<int>> &dp){
       if(idx == 0){
         if(t % v[idx] == 0) return (t / v[idx]);
@@ -17,4 +36,5 @@ public:
       int ans = f(n - 1, t, v, dp);
       return ans >= 1e9? -1 : ans;
     }
+    */
 };
