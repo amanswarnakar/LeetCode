57390cb320d584df20ee825798ccd6c7bdac48f4
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-05-03 15:59:42 +0530

    Time: 15 ms (69.57%), Space: 11.9 MB (51.85%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+    int findTargetSumWays(vector<int>& v, int tar) {
+      int n = v.size(), sum = accumulate(v.begin(), v.end(), 0);
+      if((sum - tar) % 2 or (sum - tar) < 0) return 0;
+      int t = (sum - tar) / 2;
+      vector<vector<int>> dp(n, vector<int>(t + 1, 0));
+      for(int i = 0; i < t + 1; i++){
+        if(v[0] == 0 and i == 0) dp[0][0] = 2;
+        else if(v[0] == i or i == 0) dp[0][i] = 1;
+      }
+      for(int i = 1; i < n; i++){
+        for(int j = 0; j < t + 1; j++){
+          int pick = 0;
+          if(v[i] <= j) pick = dp[i - 1][j - v[i]];
+          int notPick = dp[i - 1][j];
+          dp[i][j] = pick + notPick;
+        }
+      }
+      return dp[n - 1][t];
+    }
+  
+    /*
     int f(int idx, int t, vector<int> &v, vector<vector<int>> &dp){
       if(idx == 0){
         if(v[0] == 0 and t == 0) return 2;
@@ -20,4 +41,5 @@ public:
       vector<vector<int>> dp(n, vector<int>(t + 1, -1));
       return f(n - 1, t, v, dp);
     }
+    */
 };
