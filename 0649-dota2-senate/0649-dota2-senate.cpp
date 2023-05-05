f2729e3fed4bd7562de9c3e0a42134e96cd9df95
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-05-05 12:56:18 +0530

    Time: 14 ms (17.04%), Space: 8 MB (35.93%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    string predictPartyVictory(string senate) {
-      queue<int> q1, q2;
-      int n = senate.length();
-      for(int i = 0; i<n; i++)
-        (senate[i] == 'R')?q1.push(i):q2.push(i);
-      while(!q1.empty() && !q2.empty()){
-        int r_index = q1.front(), d_index = q2.front();
-        q1.pop(), q2.pop();
-        (r_index < d_index)?q1.push(r_index + n):q2.push(d_index + n);
+    string predictPartyVictory(string s) {
+      queue<int> r, d;
+      int n = s.size();
+      for(int i = 0; i < n; i++){
+        s[i] == 'R' ? r.emplace(i) : d.emplace(i);
       }
-      return (q1.size() > q2.size())? "Radiant" : "Dire";
+      while(!r.empty() and !d.empty()){
+        int rid = r.front(), did = d.front();
+        r.pop(); d.pop();
+        (rid < did) ? r.emplace(rid + n) : d.emplace(did + n);
+      }
+      return r.size() > d.size() ? "Radiant" : "Dire";
     }
 };
