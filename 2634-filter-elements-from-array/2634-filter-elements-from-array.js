b2bba532e1dc8a5b06d1fcdfeedcc3ae98937df6
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-05-09 22:27:50 +0530

    Time: 59 ms (51.50%), Space: 41.9 MB (60.17%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -4,5 +4,9 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    return arr.filter(fn);
+  const v = [];
+  for(let i = 0; i < arr.length; i++){
+    if(fn(arr[i], i)) v.push(arr[i]);
+  }
+  return v;
 };
