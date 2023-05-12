7453551760346cbaf67bd41c029bb43315bae00c
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-05-12 23:04:57 +0530

    Time: 400 ms (24.27%), Space: 115.2 MB (84.05%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,5 +1,19 @@
 class Solution {
-public: 
+public:
+    long long mostPoints(vector<vector<int>>& v) {
+      int n = v.size();
+      vector<long long> dp(n, 0);
+      dp[n - 1] = v[n - 1][0];
+      for(int idx = n - 2; idx >= 0; idx--){
+        long long pick = v[idx][0];
+        if(idx + 1 + v[idx][1] < n)
+          pick += dp[idx + v[idx][1] + 1];
+        long long notPick = dp[idx + 1];
+        dp[idx] = max(pick, notPick);
+      }
+      return dp[0];
+    }
+  /*
     long long f(int idx, vector<vector<int>> &v, vector<long long> &dp){
       if(idx >= v.size()) return 0;
       if(dp[idx] != -1) return dp[idx];
@@ -12,4 +26,5 @@ public:
       vector<long long> dp(n, -1);
       return f(0, v, dp);
     }
+  */
 };
