90396081a69579589bfae7dcff89c39e7e2431de
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-05-13 10:43:21 +0530

    Time: 21 ms (36.68%), Space: 9.3 MB (81.95%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
   int mod = 1e9+7;
+  int countGoodStrings(int lo, int hi, int zero, int one) {
+    vector<int> dp(hi + 1, 0);
+    dp[0] = 1;
+    for(int idx = 1; idx < hi + 1; idx++){
+      if(idx >= one) dp[idx] += dp[idx - one];
+      if(idx >= zero) dp[idx] += dp[idx - zero];
+      dp[idx] %= mod;
+    }
+    int ans = 0;
+    for(int i = lo; i < hi + 1; i++){
+      ans += dp[i];
+      ans %= mod;
+    }
+    return ans;
+  }
+  
+  /*
   int f(int idx, int zero, int one, vector<int> &dp){
     if(dp[idx] != -1) return dp[idx];
     int cnt = 0;
@@ -18,4 +35,5 @@ public:
     }
     return ans;
   }
+  */
 };
