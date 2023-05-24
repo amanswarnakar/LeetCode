1333847decfa312a51c21b64db090f0046fd2225
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-05-24 18:56:30 +0530

    Time: 106 ms (18.95%), Space: 75.6 MB (28.19%) - LeetHub

--- 2633-convert-object-to-json-string/2633-convert-object-to-json-string.js
+++ 2633-convert-object-to-json-string/2633-convert-object-to-json-string.js
@@ -3,5 +3,16 @@
  * @return {string}
  */
 var jsonStringify = function(object) {
-  return JSON.stringify(object);
+  if(object === null) return 'null';
+  if(Array.isArray(object)){
+    const elements = object.map(e => jsonStringify(e));
+    return `[${elements.join(',')}]`;
+  }
+  if(typeof object === 'object'){
+    const keys = Object.keys(object);
+    const keyValuePairs = keys.map(key => `"${key}":${jsonStringify(object[key])}`);
+    return `{${keyValuePairs.join(',')}}`;
+  }
+  if(typeof object === 'string') return `"${object}"`;
+  return String(object);
 };
