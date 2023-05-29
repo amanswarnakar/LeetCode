d1a56c83f79ab80278e8bf20826cceac5155c987
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2023-05-29 18:42:17 +0530

    Time: 111 ms (81.30%), Space: 61.4 MB (88.43%) - LeetHub

--- 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -3,13 +3,27 @@
  * @param {number} depth
  * @return {any[]}
  */
+
 var flat = function (arr, n) {
-  if(n === 0) return arr;
   const res = [];
-  for(const num of arr){
-    if(Array.isArray(num))
-      res.push(...flat(num, n - 1));
-    else res.push(num);
+  const flattening = (nums, l) => {
+    for(const num of nums){
+      if(Array.isArray(num) && l > 0)
+        flattening(num, l - 1);
+      else res.push(num);
+    }
   }
+  flattening(arr, n);
   return res;
 };
+
+// var flat = function (arr, n) {
+//   if(n === 0) return arr;
+//   const res = [];
+//   for(const num of arr){
+//     if(Array.isArray(num))
+//       res.push(...flat(num, n - 1));
+//     else res.push(num);
+//   }
+//   return res;
+// };
